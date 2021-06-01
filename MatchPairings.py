cc8db8ba7891304b6e14632f07bc08f1edcf92f8
Tiashka <seetaltiashka@gmail.com>
2021-06-01 23:33:09 +0200

    formatted the __str__ function for printing food items

--- MatchPairings.py
+++ MatchPairings.py
@@ -1,10 +1,11 @@
 from MockFoodItem import FoodItem
 burger=FoodItem("Burger","mcDonalds")
-print(burger.location)
+print(burger)
 macAndCheese=FoodItem("Mac and Cheese","Panarottis")
 sushi=FoodItem("sushi","John Dorys")
 
 foodArray=[burger,macAndCheese,sushi]
+print("Food Array:")
 for i in foodArray:
-    print(i.food," ",i.location," ")
+    print(i)
 print(foodArray)
--- MockFoodItem.py
+++ MockFoodItem.py
@@ -2,3 +2,6 @@ class FoodItem:
     def __init__(self,food,location):
         self.food=food
         self.location=location
+
+    def  __str__(self): #used to format the print()
+        return "Name: "+str(self.food)+" Location: "+str(self.location)
