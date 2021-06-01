6b8aec0893bd68b44e80cd38b181f61fb92969d4
Tiashka <seetaltiashka@gmail.com>
2021-06-01 23:28:14 +0200

    tested one food arrays

--- MatchPairings.py
+++ MatchPairings.py
@@ -1,3 +1,10 @@
 from MockFoodItem import FoodItem
 burger=FoodItem("Burger","mcDonalds")
 print(burger.location)
+macAndCheese=FoodItem("Mac and Cheese","Panarottis")
+sushi=FoodItem("sushi","John Dorys")
+
+foodArray=[burger,macAndCheese,sushi]
+for i in foodArray:
+    print(i.food," ",i.location," ")
+print(foodArray)
--- MockFoodItem.py
+++ MockFoodItem.py
@@ -2,4 +2,3 @@ class FoodItem:
     def __init__(self,food,location):
         self.food=food
         self.location=location
-
