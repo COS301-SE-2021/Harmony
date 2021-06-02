8e1fb94cfde3fc0565d691c09c9d8ddd78ba3046
MohammedCarrim <u18080813@tuks.co.za>
2021-06-03 00:26:26 +0200

    added imagerek.py for testing

---
+++ imagerek.py
@@ -0,0 +1,13 @@
+import boto3
+
+if __name__== "__main__":
+    bucket= 'imagesdreamteam'
+    photo = 'images/coca-cola-original-20oz.png'
+    modelarn = 'arn:aws:rekognition:us-east-2:294481406452:project/foodimage/version/foodimage.2021-06-02T00.31.13/1622586673511'
+client = boto3.client('rekognition')
+
+response = client.detect_custom_labels(ProjectVersionArn = modelarn, Image = {'S3Object': {"Bucket": bucket,"Name": photo}})
+
+for data in response['CustomLabels']:
+    print(data['Name'])
+    print(data['Confidence'])
