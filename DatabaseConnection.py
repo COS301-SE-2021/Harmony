acf054cbc30c387ed231d88b65bff1896cbc8e1f
Tiashka <seetaltiashka@gmail.com>
2021-06-02 22:38:34 +0200

    created db link

---
+++ DatabaseConnection.py
@@ -0,0 +1,34 @@
+import boto3
+
+
+def createFoodItemTable(dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
+        # remove endpoint to use ddb in aws endpoint is to tell local db
+
+    table = dynamodb.create_table(
+        TableName='FoodItem',
+        KeySchema=[
+            {
+                'AttributeName': 'ID',
+                'KeyType': 'HASH'  # Sort key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'ID',
+                'AttributeType': 'N'
+            },
+
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+    )
+    return table
+
+
+if __name__ == '__main__':
+    foodItemTable = createFoodItemTable()
+    print("Table status:", foodItemTable.table_status)
