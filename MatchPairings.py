911cbb7e79c497082cc339df5bb17b8107698623
Tiashka <seetaltiashka@gmail.com>
2021-06-02 08:44:21 +0200

    added the mock database +append and __str__

--- MatchPairings.py
+++ MatchPairings.py
@@ -1,11 +1,17 @@
-from MockFoodItem import FoodItem
+from MockFoodItem import FoodItem,FoodItemDatabase
+
 burger=FoodItem("Burger","mcDonalds")
 print(burger)
 macAndCheese=FoodItem("Mac and Cheese","Panarottis")
 sushi=FoodItem("sushi","John Dorys")
 
 foodArray=[burger,macAndCheese,sushi]
-print("Food Array:")
+print("Food Array:\n")
 for i in foodArray:
     print(i)
-print(foodArray)
+
+database=FoodItemDatabase(foodArray)
+spaghetti=FoodItem("Spaghetti Bolognaise","Mimmos")
+database.add(spaghetti)
+print("Check database\n\n")
+print(database)
--- MockFoodItem.py
+++ MockFoodItem.py
@@ -5,3 +5,23 @@ class FoodItem:
 
     def  __str__(self): #used to format the print()
         return "Name: "+str(self.food)+" Location: "+str(self.location)
+
+class FoodItemDatabase:
+    data=[]
+    def __init__(self,data):
+        self.data=data
+
+    def add(self,fooditem):
+        self.data.append(fooditem)
+
+    def search(self,name):
+        for i in self.data:
+            if i.food==name:
+                return i
+
+    def __str__(self):
+        result=""
+        for i in self.data:
+            result=result+str(i.food)+" found at "+str(i.location)+" \n"
+        return result
+
