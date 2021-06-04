b9e7ff10636be078cd45b7b916fa9b2347fae15c
MohammedCarrim <u18080813@tuks.co.za>
2021-06-04 14:57:06 +0200

    Edited response output in IdentifyFoodItem.py

--- backend/src/main/java/imageRecognition/IdentifyFoodItem.py
+++ backend/src/main/java/imageRecognition/IdentifyFoodItem.py
@@ -1,17 +1,19 @@
 import boto3
 
 if __name__ == "__main__":
-    # Reference to bucket id
+    # Load image from S3 bucket
     bucket = 'imagesdreamteam'
     # Reference to the image path in bucket(storage)
-    photo = 'images/free-png-ywwqo-f350x360.png'
+    photo = 'images/sprite test.png'
     # Reference to trained datamodel
     # arn - amazon resource name
     modelarn = 'arn:aws:rekognition:us-east-2:294481406452:project/foodimage/version/foodimage.2021-06-02T00.31.13/1622586673511'
 client = boto3.client('rekognition')
 
+# Call DetectCustomLabels
 response = client.detect_custom_labels(ProjectVersionArn = modelarn , Image = {'S3Object': {"Bucket": bucket,"Name": photo}})
 
 for data in response['CustomLabels']:
     # Print what is in the response
-    print("This is a " + str(data['Name']))
+        print('Label ' + str(data['Name']))
+        print('Confidence ' + str(data['Confidence']))
