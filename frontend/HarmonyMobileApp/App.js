c7e52e5ae7b2c48d230ef711193046072c4d17d3
Jd Perumal <u18219285@tuks.co.za>
2021-06-18 20:15:05 +0200

    Basic Login UI

--- frontend/HarmonyMobileApp/App.js
+++ frontend/HarmonyMobileApp/App.js
@@ -1,21 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  Text, View,  
+  TextInput,
+  Button,
+  TouchableOpacity
+ } from 'react-native';
 
+import styles from './styles'
 export default function App() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Lets goooooooo!</Text>
       <StatusBar style="auto" />
+
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email:"
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>   
+
+      <View style={styles.inputView}>
+      <TextInput
+        style={styles.TextInput}
+        placeholder="Password:"
+        placeholderTextColor="#003f5c"
+        secureTextEntry={true}
+        onChangeText={(password) => setPassword(password)}
+      />
+      </View>
+
+      <TouchableOpacity>
+      <Text style={styles.forgot_button}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.loginBtn}>
+      <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
     </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
