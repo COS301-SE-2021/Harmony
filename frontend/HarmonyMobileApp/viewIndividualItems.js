0a5801ab0447252381a6a019782413bd8850d4ed
Tiashka <seetaltiashka@gmail.com>
2021-06-19 01:42:11 +0200

    changed view layout

--- frontend/HarmonyMobileApp/viewIndividualItems.js
+++ frontend/HarmonyMobileApp/viewIndividualItems.js
@@ -4,13 +4,17 @@ import styles from './styles'
 
 const ViewIndividualItems =(props)=>{
     return(
-        <SafeAreaView style={styles.container}> 
-        <Text style={styles.TextLarge}>View Individual Pairing</Text>
+        <SafeAreaView>
+        <SafeAreaView style={styles.container,{flex:0,paddingTop:20}}> 
+        <Text style={styles.TextLarge }>View Individual Pairing</Text>
+        </SafeAreaView>
+        <View style={styles.container,{position:"relative",alignSelf:"center",}}>
         <Image source={require("./assets/waffles.jpg")} style={styles.regularImage}/>
         <Text style={styles.TextMedium}> Waffles </Text>
         <Image source={require("./assets/milkshake.jpg")} style={styles.regularImage}/>
         <Text style={styles.TextSmall}> A Milkshake </Text>
 
+        </View>
         </SafeAreaView>
     )
 };
