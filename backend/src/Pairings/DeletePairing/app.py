5423728a8f2ba34953ca761acd27e51ae5ed1fdc
MohammedCarrim <u18080813@tuks.co.za>
2021-06-19 00:54:00 +0200

    Modified code DeletePairing

--- backend/src/Pairings/DeletePairing/app.py
+++ backend/src/Pairings/DeletePairing/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "hello world pairings",
             # "location": ip.text.replace("\n", "")
         }),
     }
