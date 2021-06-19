2827c7a4e991a2e7623ec3e6dfacdb867f991c47
MohammedCarrim <u18080813@tuks.co.za>
2021-06-19 13:10:44 +0200

    Fixed request input for deleteParingsLambda

--- backend/src/Pairings/DeletePairing/app.py
+++ backend/src/Pairings/DeletePairing/app.py
@@ -18,7 +18,7 @@ now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 # define the handler function that the Lambda service will use as an entry point
 def lambda_handler(event, context):
 # extract values from the event object we got from the Lambda service and store in a variable
-    pid = event['pid']
+    pid = event['PID']
     try:
         response = table.delete_item(
             Key={
