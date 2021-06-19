e79e222f9f4be943e38e0222cd4df4250f0e9de3
MohammedCarrim <u18080813@tuks.co.za>
2021-06-19 13:02:17 +0200

    Added Basic Delete in DeletePairing/app.py

--- backend/src/Pairings/DeletePairing/app.py
+++ backend/src/Pairings/DeletePairing/app.py
@@ -1,42 +1,35 @@
+# import the json utility package since we will be working with a JSON object
 import json
+from decimal import Decimal
 
-# import requests
+# import the AWS SDK (for Python the package name is boto3)
+import boto3
+# import two packages to help us with dates and date formatting
+from time import gmtime, strftime
+from botocore.exceptions import ClientError
 
+# create a DynamoDB object using the AWS SDK
+dynamodb = boto3.resource('dynamodb')
+# use the DynamoDB object to select our table
+table = dynamodb.Table('Pairings')
+# store the current time in a human readable format in a variable
+now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 
+# define the handler function that the Lambda service will use as an entry point
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+# extract values from the event object we got from the Lambda service and store in a variable
+    pid = event['pid']
+    try:
+        response = table.delete_item(
+            Key={
+                'PID': pid
+            }
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == "ConditionalCheckFailedException":
+            print(e.response['Error']['Message'])
+        else:
+            raise
+    else:
+        return response
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world pairings",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
