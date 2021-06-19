4be292bb91c41136a5953db856eb87f76dec221f
Tiashka <seetaltiashka@gmail.com>
2021-06-19 18:16:41 +0200

    Update styles.js

--- frontend/HarmonyMobileApp/styles.js
+++ frontend/HarmonyMobileApp/styles.js
@@ -6,6 +6,7 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+      position:"absolute"
     },
   
     inputView: {
@@ -39,6 +40,42 @@ const styles = StyleSheet.create({
       marginTop: 40,
       backgroundColor: "#20b2aa",
     },
+
+    backgroundBarShowLatest: {
+      borderRadius: 25,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      position:"relative",
+      backgroundColor: "#D4D3D3",
+      padding:"5%"
+    },
+    Text:{
+      fontFamily:"sans-serif-light"
+    },
+
+    TextLarge:{
+      fontFamily:"sans-serif-light",
+      fontSize: 30, 
+    },
+    TextMedium:{
+      fontFamily:"sans-serif-light",
+      fontSize: 23, 
+    },
+    TextSmall:{
+      fontFamily:"sans-serif-light",
+      fontSize:14
+    },
+    regularImage:{
+      width:150,
+      height:100,
+      resizeMode:"contain"
+    },
+    smallImage:{
+      width:50,
+      height:50,
+      resizeMode:"contain"
+    }
 });
   
 export default styles;
