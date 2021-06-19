c03950ba76c5140d2805399340e460ec827608e7
Keval157 <u18012559@tuks.co.za>
2021-06-20 00:13:41 +0200

    Extracting values from event object

--- backend/src/Pairings/NewPairing/app.py
+++ backend/src/Pairings/NewPairing/app.py
@@ -1,42 +1,31 @@
+# import the json utility package since we will be working with a JSON object
 import json
 
-# import requests
+# import the AWS SDK (for Python the package name is boto3)
+import boto3
+
+# import two packages to help us with dates and date formatting
+from time import gmtime, strftime
+
+dynamodb = boto3.resource('dynamodb')
+
+# use the DynamoDB object to select our table
+table = dynamodb.Table('Pairings')
+
+# store the current time in a human readable format in a variable
+now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 
 
+# define the handler function that the Lambda service will use as an entry point
 def lambda_handler(event, context):
-    """Sample pure Lambda function
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    a = event['PID']
+    b = event['DrinkDesc']
+    c = event['DrinkItem']
+    d = event['FoodDesc']
+    e = event['FoodItem']
+    f = event['Location']
+    g = event['UID']
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
 
-    context: object, required
-        Lambda Context runtime methods and attributes
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello Keval",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
