bee14335572f1c89ed446e539bdbef593c452deb
Arshad Sacoor <arshadsacoor@me.com>
2021-06-19 14:18:06 +0200

    ViewPairings Function Updated

--- backend/src/Pairings/ViewPairings/app.py
+++ backend/src/Pairings/ViewPairings/app.py
@@ -1,42 +1,18 @@
 import json
+import boto3
 
-# import requests
+client = boto3.resource('dynamodb')
+table = client.Table('Pairings')
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    print("Hello Harmony")
+    product_number = "p1"
+    print(product_number)
+    response = table.get_item(
+        Key={
+            'PID': product_number
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+        }
+    )
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
