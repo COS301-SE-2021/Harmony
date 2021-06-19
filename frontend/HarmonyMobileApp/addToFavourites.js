643b566b2ff7803823c951703a24e73d0c1b8c66
Tiashka <seetaltiashka@gmail.com>
2021-06-19 10:36:24 +0200

    changed layout

--- frontend/HarmonyMobileApp/addToFavourites.js
+++ frontend/HarmonyMobileApp/addToFavourites.js
@@ -4,24 +4,98 @@ import styles from './styles'
 
 const AddToFavourites =(props)=>{
     return(
-        <SafeAreaView style={styles.container}>
-            <SafeAreaView style={styles.container,{flex:0,paddingTop:20}}> 
+        <SafeAreaView style={personalStyles.container}>
+        <View style={{paddingTop:"20%"}}> 
             <Text style={styles.TextLarge }>Favourites</Text>
-            </SafeAreaView>
-            <View style={styles.container,{justifyContent:"center",flexDirection:"row",marginTop:25}}>
-                <View>
-                    <Image source={require("./assets/waffles.jpg")} style={styles.regularImage}/>
+        </View>
+        <ScrollView style={personalStyles.scrollView}>
+            <View style={styles.backgroundBarShowLatest}>
+                <View style={{flexDirection:"row",alignItems:"center"}}>
                     <Text style={styles.TextMedium}> Waffles </Text>
+                    <View style={{justifyContent:"center"}}>
+                        <Image source={require("./assets/plus.png")} style={styles.smallImage}/>
+                    </View>
+                    <Text style={styles.TextMedium}> A Milkshake </Text>
                 </View>
-                <View style={{justifyContent:"center"}}>
-                    <Image source={require("./assets/plus.png")} style={styles.smallImage}/>
-                </View>
-                <View>
-            <Image source={require("./assets/milkshake.jpg")} style={styles.regularImage}/>
-            <Text style={styles.TextSmall}> A Milkshake </Text>
+                <View style={{flexDirection:"row",alignItems:"center"}}>
+                    <View style={{justifyContent:"center",}}>
+                        <Image source={require("./assets/location.png")} style={{ width:30, height:30, resizeMode:"contain"}}/>
+                    </View>
+                    <View>
+                        <Text style={styles.TextSmall}> Waffle House, Ramsgate, South Coast</Text>
+                    </View>
                 </View>
             </View>
-        </SafeAreaView>
+            <View style={styles.backgroundBarShowLatest}>
+                <View style={{flexDirection:"row",alignItems:"center"}}>
+                    <Text style={styles.TextMedium}> Burgers </Text>
+                    <View style={{justifyContent:"center"}}>
+                        <Image source={require("./assets/plus.png")} style={styles.smallImage}/>
+                    </View>
+                    <Text style={styles.TextMedium}> Coke </Text>
+                </View>
+                <View style={{flexDirection:"row",alignItems:"center"}}>
+                    <View style={{justifyContent:"center",}}>
+                        <Image source={require("./assets/location.png")} style={{ width:30, height:30, resizeMode:"contain"}}/>
+                    </View>
+                    <View>
+                        <Text style={styles.TextSmall}> Rocomamas, Gateway, Umhlanga</Text>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.backgroundBarShowLatest}>
+                <View style={{flexDirection:"row",alignItems:"center"}}>
+                    <Text style={styles.TextMedium}> French Toast </Text>
+                    <View style={{justifyContent:"center"}}>
+                        <Image source={require("./assets/plus.png")} style={styles.smallImage}/>
+                    </View>
+                    <Text style={styles.TextMedium}> Coffee </Text>
+                </View>
+                <View style={{flexDirection:"row",alignItems:"center"}}>
+                    <View style={{justifyContent:"center",}}>
+                        <Image source={require("./assets/location.png")} style={{ width:30, height:30, resizeMode:"contain"}}/>
+                    </View>
+                    <View>
+                        <Text style={styles.TextSmall}> 1855, Lynnwood, Pretoria</Text>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.backgroundBarShowLatest}>
+                <View style={{flexDirection:"row",alignItems:"center"}}>
+                    <Text style={styles.TextMedium}> Bunny Chow </Text>
+                    <View style={{justifyContent:"center"}}>
+                        <Image source={require("./assets/plus.png")} style={styles.smallImage}/>
+                    </View>
+                    <Text style={styles.TextMedium}> Coke </Text>
+                </View>
+                <View style={{flexDirection:"row",alignItems:"center"}}>
+                    <View style={{justifyContent:"center",}}>
+                        <Image source={require("./assets/location.png")} style={{ width:30, height:30, resizeMode:"contain"}}/>
+                    </View>
+                    <View>
+                        <Text style={styles.TextSmall}> 4 Chilli, Garsfontein, Pretoria</Text>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.backgroundBarShowLatest}>
+                <View style={{flexDirection:"row",alignItems:"center"}}>
+                    <Text style={styles.TextMedium}> Koeksister </Text>
+                    <View style={{justifyContent:"center"}}>
+                        <Image source={require("./assets/plus.png")} style={styles.smallImage}/>
+                    </View>
+                    <Text style={styles.TextMedium}> Tea </Text>
+                </View>
+                <View style={{flexDirection:"row",alignItems:"center"}}>
+                    <View style={{justifyContent:"center",}}>
+                        <Image source={require("./assets/location.png")} style={{ width:30, height:30, resizeMode:"contain"}}/>
+                    </View>
+                    <View>
+                        <Text style={styles.TextSmall}> Bakehouse, HazelWood, Pretoria</Text>
+                    </View>
+                </View>
+            </View>
+        </ScrollView>
+    </SafeAreaView>
     )
 };
 const personalStyles = StyleSheet.create({
