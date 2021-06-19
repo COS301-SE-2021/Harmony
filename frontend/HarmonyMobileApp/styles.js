fd4d15cf07c5712c0fa474f4eeb8d810e0e9c0ba
Tiashka <seetaltiashka@gmail.com>
2021-06-19 14:44:23 +0200

    Update styles.js

--- frontend/HarmonyMobileApp/styles.js
+++ frontend/HarmonyMobileApp/styles.js
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
       justifyContent: "center",
       marginTop: 40,
       position:"relative",
-      backgroundColor: "grey",
+      backgroundColor: "#D4D3D3",
       padding:"5%"
     },
     Text:{
