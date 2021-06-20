99a5ed6f19ac2440fda73b9ac238dcb772b80214
MohammedCarrim <u18080813@tuks.co.za>
2021-06-21 01:20:45 +0200

    added documentation to viewpairings

--- backend/src/Pairings/ViewPairings/app.py
+++ backend/src/Pairings/ViewPairings/app.py
@@ -8,7 +8,9 @@ table = client.Table('Pairings')
 
 def lambda_handler(event, context):
     print("Hello Harmony")
+    # add entire table to allresponse
     allresponse = table.scan()
+    # edit the response to only show items
     response = allresponse['Items']
     # response = table.query(
     #     KeyConditionExpression=
@@ -58,7 +60,7 @@ def lambda_handler(event, context):
     # }
 
     return {
-
+        # returns all items stored in response
         "StatusCode": 200,
         "Data": response
     }
