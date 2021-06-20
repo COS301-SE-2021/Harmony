f38bb7671c8545392a48f2dc6ceebddc613dbf00
MohammedCarrim <u18080813@tuks.co.za>
2021-06-21 01:18:48 +0200

    added dynamic search for viewpairings

--- backend/src/Pairings/ViewPairings/app.py
+++ backend/src/Pairings/ViewPairings/app.py
@@ -8,49 +8,57 @@ table = client.Table('Pairings')
 
 def lambda_handler(event, context):
     print("Hello Harmony")
-    response = table.query(
-        KeyConditionExpression=
-        Key('PID').eq('p1')
-    )
-    items1 = response['Items']
-    print(items1)
-    response = table.query(
-        KeyConditionExpression=
-        Key('PID').eq('p2')
-    )
-    items2 = response['Items']
-    print(items2)
-    response = table.query(
-        KeyConditionExpression=
-        Key('PID').eq('p3')
-    )
-    items3 = response['Items']
-    print(items3)
-
-    response = table.query(
-        KeyConditionExpression=
-        Key('PID').eq('p4')
-    )
-    items4 = response['Items']
-    print(items4)
-
-    response = table.query(
-        KeyConditionExpression=
-        Key('PID').eq('p5')
-    )
-    items5 = response['Items']
-    print(items5)
-
-    response = table.query(
-        KeyConditionExpression=
-        Key('PID').eq('p6')
-    )
-    items6 = response['Items']
-    print(items6)
+    allresponse = table.scan()
+    response = allresponse['Items']
+    # response = table.query(
+    #     KeyConditionExpression=
+    #     Key('PID').eq('p1')
+    # )
+    # items1 = response['Items']
+    # print(items1)
+    # response = table.query(
+    #     KeyConditionExpression=
+    #     Key('PID').eq('p2')
+    # )
+    # items2 = response['Items']
+    # print(items2)
+    # response = table.query(
+    #     KeyConditionExpression=
+    #     Key('PID').eq('p3')
+    # )
+    # items3 = response['Items']
+    # print(items3)
+    #
+    # response = table.query(
+    #     KeyConditionExpression=
+    #     Key('PID').eq('p4')
+    # )
+    # items4 = response['Items']
+    # print(items4)
+    #
+    # response = table.query(
+    #     KeyConditionExpression=
+    #     Key('PID').eq('p5')
+    # )
+    # items5 = response['Items']
+    # print(items5)
+    #
+    # response = table.query(
+    #     KeyConditionExpression=
+    #     Key('PID').eq('p6')
+    # )
+    # items6 = response['Items']
+    # print(items6)
+    #
+    # return {
+    #
+    #     "StatusCode": 200,
+    #
+    #     "Data": items6 + items5 + items4 + items3 + items2 + items1
+    # }
 
     return {
 
         "StatusCode": 200,
-
-        "Data": items6 + items5 + items4 + items3 + items2 + items1
+        "Data": response
     }
