04151b349a4ba4e56f26cc9b4007382f1b5e8f87
Jd Perumal <u18219285@tuks.co.za>
2021-06-21 04:27:19 +0200

    Fixed virtualized list warning

--- frontend/HarmonyMobileApp/screens/ViewAllPairingsScreen.js
+++ frontend/HarmonyMobileApp/screens/ViewAllPairingsScreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect,useState } from "react";
-import { useIsFocused } from "@react-navigation/native";  
+import React, { useEffect, useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
 import { render } from "react-dom";
 import {
   View,
@@ -17,19 +17,19 @@ import {
 import styles from "../styles";
 
 const ViewAllPairingsScreen = (props) => {
-    const viewPairingURL =
-      "https://qkvdftfq7b.execute-api.eu-west-1.amazonaws.com/dev/viewpairings";
-    const [isLoading, setLoading] = useState(useIsFocused());
-    const [data, setData] = useState([]);
-  
-    useEffect(() => {
-      fetch(viewPairingURL)
-        .then((response) => response.json())
-        .then((json) => setData(json.Data))
-        .catch((error) => alert(error))
-        .then(setLoading(false));
-    });
-    
+  const viewPairingURL =
+    "https://qkvdftfq7b.execute-api.eu-west-1.amazonaws.com/dev/viewpairings";
+  const [isLoading, setLoading] = useState(useIsFocused());
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(viewPairingURL)
+      .then((response) => response.json())
+      .then((json) => setData(json.Data))
+      .catch((error) => alert(error))
+      .then(setLoading(false));
+  });
+
   const showConfirmDialog = () => {
     return Alert.alert(
       "Add to Favourites",
@@ -51,94 +51,86 @@ const ViewAllPairingsScreen = (props) => {
     );
   };
 
-
-  return(
-      <SafeAreaView style={personalStyles.container}>
-        <ScrollView style={personalStyles.scrollView}>
-          <View style={styles.backgroundBarShowLatest}>
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <View style={{ justifyContent: "center" }}>
-              <Text style={styles.TextLarge}> Popular Pairings </Text>
-              </View>
-            </View>
-          </View>
-          <View>
-{isLoading?(
-  <ActivityIndicator/>
-):(
-  <FlatList
-    data={data}
-    keyExtractor={({PID},index)=>PID}
-    renderItem={({item})=>(
+  return (
+    <SafeAreaView style={personalStyles.container}>
       <View style={styles.backgroundBarShowLatest}>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "flex-start",
-              }}
-            >
-              <View>
-                <Image
-                  source={require("../assets/person.png")}
-                  style={{ width: 40, height: 40, resizeMode: "contain" }}
-                />
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View style={{ justifyContent: "center" }}>
+            <Text style={styles.TextLarge}> Popular Pairings </Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <FlatList
+            data={data}
+            keyExtractor={({ PID }, index) => PID}
+            renderItem={({ item }) => (
+              <View style={styles.backgroundBarShowLatest}>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "flex-start",
+                  }}
+                >
+                  <View>
+                    <Image
+                      source={require("../assets/person.png")}
+                      style={{ width: 40, height: 40, resizeMode: "contain" }}
+                    />
+                  </View>
+                  <View>
+                    <Text style={styles.TextSmall}> {item.UID} </Text>
+                  </View>
+                </View>
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <Text style={styles.TextMedium}> {item.FoodItem} </Text>
+                  <View style={{ justifyContent: "center" }}>
+                    <Image
+                      source={require("../assets/plus.png")}
+                      style={styles.smallImage}
+                    />
+                  </View>
+                  <Text style={styles.TextMedium}> {item.DrinkItem} </Text>
+                </View>
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <View style={{ justifyContent: "center" }}>
+                    <Image
+                      source={require("../assets/location.png")}
+                      style={{ width: 30, height: 30, resizeMode: "contain" }}
+                    />
+                  </View>
+                  <View>
+                    <Text style={styles.TextSmall}>{item.Location}</Text>
+                  </View>
+                </View>
+                <TouchableOpacity style={personalStyles.addToFavouriteBtn}>
+                  <View style={{ justifyContent: "center" }}>
+                    <Image
+                      source={require("../assets/favourites.png")}
+                      style={{ width: 40, height: 40, resizeMode: "contain" }}
+                    />
+                  </View>
+                  <View>
+                    <Text style={styles.TextSmall}> Add to favourites</Text>
+                  </View>
+                </TouchableOpacity>
               </View>
-              <View>
-                <Text style={styles.TextSmall}> {item.UID} </Text>
-              </View>
-            </View>
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <Text style={styles.TextMedium}> {item.FoodItem}  </Text>
-              <View style={{ justifyContent: "center" }}>
-                <Image
-                  source={require("../assets/plus.png")}
-                  style={styles.smallImage}
-                />
-              </View>
-              <Text style={styles.TextMedium}> {item.DrinkItem} </Text>
-            </View>
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <View style={{ justifyContent: "center" }}>
-                <Image
-                  source={require("../assets/location.png")}
-                  style={{ width: 30, height: 30, resizeMode: "contain" }}
-                />
-              </View>
-              <View>
-                <Text style={styles.TextSmall}>
-                  {item.Location}
-                </Text>
-              </View>
-            </View>
-            <TouchableOpacity
-              style={personalStyles.addToFavouriteBtn}
-            >
-              <View style={{ justifyContent: "center" }}>
-                <Image
-                  source={require("../assets/favourites.png")}
-                  style={{ width: 40, height: 40, resizeMode: "contain" }}
-                />
-              </View>
-              <View>
-                <Text style={styles.TextSmall}> Add to favourites</Text>
-              </View>
-            </TouchableOpacity>
-          </View>    
-    
-    
-    )}
-/>
-)}</View>
-         
-        </ScrollView>
-      </SafeAreaView>
-    );
+            )}
+          />
+        )}
+      </View>
+    </SafeAreaView>
+  );
 };
 const personalStyles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: StatusBar.currentHeight,
+    paddingBottom: 130,
   },
   scrollView: {
     marginHorizontal: 20,
--- frontend/HarmonyMobileApp/screens/ViewFavouritesScreen.js
+++ frontend/HarmonyMobileApp/screens/ViewFavouritesScreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect,useState } from "react";
-import { useIsFocused } from "@react-navigation/native";  
+import React, { useEffect, useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
 import {
   View,
   Text,
@@ -10,9 +10,8 @@ import {
   StatusBar,
   TouchableOpacity,
   Alert,
-  FlatList
+  FlatList,
 } from "react-native";
-
 
 import {
   FontAwesome,
@@ -25,55 +24,55 @@ import {
 import styles from "../styles";
 
 const ViewFavouritesScreen = (props) => {
-  const viewFavouritesURL ="https://ypveh68wo0.execute-api.eu-west-1.amazonaws.com/dev";
-  const deleteFavourite="https://op7td19wdb.execute-api.eu-west-1.amazonaws.com/dev";
-const [isLoading, setLoading] = useState(useIsFocused());
-const [data, setData] = useState([]);
+  const viewFavouritesURL =
+    "https://ypveh68wo0.execute-api.eu-west-1.amazonaws.com/dev";
+  const deleteFavourite =
+    "https://op7td19wdb.execute-api.eu-west-1.amazonaws.com/dev";
+  const [isLoading, setLoading] = useState(useIsFocused());
+  const [data, setData] = useState([]);
 
-useEffect(() => {
-  fetch(viewFavouritesURL,{
-    method:"POST",
-    headers:{
-      Accept:"application/json",
-      "Content-Type":"application/json"
-    },
-    body:JSON.stringify({
-      "UID":"u1"
+  useEffect(() => {
+    fetch(viewFavouritesURL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        UID: "u1",
+      }),
     })
-  })
-    .then((response) => response.json())
-    .then((json) => setData(json.Data))
-    .catch((error) => alert(error))
-    .then(setLoading(false));
-});
+      .then((response) => response.json())
+      .then((json) => setData(json.Data))
+      .catch((error) => alert(error))
+      .then(setLoading(false));
+  });
 
   const showConfirmDialog = (pid) => {
     return Alert.alert(
       "Delete",
       "Are you sure you want to remove this pairing from favourites?",
       [
-       
         // The "No" button
         // Does nothing but dismiss the dialog when tapped
         {
           text: "No",
         },
-         // The "Yes" button
-         {
+        // The "Yes" button
+        {
           text: "Yes",
           onPress: () => {
-            fetch(deleteFavourite,{
-              method:"POST",
-              headers:{
-                Accept:"application/json",
-                "Content-Type":"application/json"
+            fetch(deleteFavourite, {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
               },
-              body:JSON.stringify({
-                "UID":"u1",
-                "PID":pid
-              })
-            })
-              .catch((error) => alert(error))
+              body: JSON.stringify({
+                UID: "u1",
+                PID: pid,
+              }),
+            }).catch((error) => alert(error));
             //setShowBox(false);
           },
         },
@@ -83,56 +82,50 @@ useEffect(() => {
   return (
     <SafeAreaView style={personalStyles.container}>
       <View style={{ paddingBottom: "2%", paddingRight: "0.5%" }}>
-        <ScrollView style={personalStyles.scrollView}>
-          <View style={{ paddingTop: "0%" }}>
-            <View style={styles.backgroundBarShowLatest}>
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
-                <View style={{ justifyContent: "center" }}>
-                  <Text style={styles.TextLarge}> My favourites </Text>
-                </View>
+        <View style={{ paddingTop: "0%" }}>
+          <View style={styles.backgroundBarShowLatest}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <View style={{ justifyContent: "center" }}>
+                <Text style={styles.TextLarge}> My favourites </Text>
               </View>
             </View>
           </View>
-          <FlatList
-    data={data}
-    keyExtractor={({PID},index)=>PID}
-    renderItem={({item})=>( <View style={styles.backgroundBarShowLatest}>
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Text style={styles.TextMedium}> {item.FoodItem} </Text>
-        <View style={{ justifyContent: "center" }}>
-          <Image
-            source={require("../assets/plus.png")}
-            style={styles.smallImage}
-          />
         </View>
-        <Text style={styles.TextMedium}> {item.DrinkItem} </Text>
-        <TouchableOpacity
-        style={personalStyles.addToFavouriteBtn}
-        onPress={() => showConfirmDialog(item.PID)}
-      >
-        <AntDesign name="minuscircleo" size={30} color="red" />
-      </TouchableOpacity>
-      </View>
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <View style={{ justifyContent: "center" }}>
-          <Image
-            source={require("../assets/location.png")}
-            style={{ width: 30, height: 30, resizeMode: "contain" }}
-          />
-        </View>
-        <View>
-          <Text style={styles.TextSmall}>
-            {" "}
-            {item.Location}
-          </Text>
-        </View>
-      </View>
-     
-    </View>)}
-    />
-         
-        
-        </ScrollView>
+        <FlatList
+          data={data}
+          keyExtractor={({ PID }, index) => PID}
+          renderItem={({ item }) => (
+            <View style={styles.backgroundBarShowLatest}>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Text style={styles.TextMedium}> {item.FoodItem} </Text>
+                <View style={{ justifyContent: "center" }}>
+                  <Image
+                    source={require("../assets/plus.png")}
+                    style={styles.smallImage}
+                  />
+                </View>
+                <Text style={styles.TextMedium}> {item.DrinkItem} </Text>
+                <TouchableOpacity
+                  style={personalStyles.addToFavouriteBtn}
+                  onPress={() => showConfirmDialog(item.PID)}
+                >
+                  <AntDesign name="minuscircleo" size={30} color="red" />
+                </TouchableOpacity>
+              </View>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <View style={{ justifyContent: "center" }}>
+                  <Image
+                    source={require("../assets/location.png")}
+                    style={{ width: 30, height: 30, resizeMode: "contain" }}
+                  />
+                </View>
+                <View>
+                  <Text style={styles.TextSmall}> {item.Location}</Text>
+                </View>
+              </View>
+            </View>
+          )}
+        />
       </View>
     </SafeAreaView>
   );
@@ -141,8 +134,9 @@ const personalStyles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: StatusBar.currentHeight,
+    paddingBottom: 130,
   },
-  secondContainer:{
+  secondContainer: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
