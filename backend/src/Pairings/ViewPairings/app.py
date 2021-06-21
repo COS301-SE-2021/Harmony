7df176b5433bd33eb876ba334893181c2ebab630
Arshad Sacoor <arshadsacoor@me.com>
2021-06-21 04:36:30 +0200

    Remove old function Viewpairing:  Lambda to API to DB Function

--- backend/src/Pairings/ViewPairings/app.py
+++ backend/src/Pairings/ViewPairings/app.py
@@ -12,39 +12,6 @@ def lambda_handler(event, context):
     allresponse = table.scan()
     # edit the response to only show items
     response = allresponse['Items']
-    # response = table.query(
-    #     KeyConditionExpression=
-    #     Key('PID').eq('p1')
-    # )
-    # items1 = response['Items']
-    # print(items1)
-    # response = table.query(
-    #     KeyConditionExpression=
-    #     Key('PID').eq('p2')
-    # )
-    # items2 = response['Items']
-    # print(items2)
-    # response = table.query(
-    #     KeyConditionExpression=
-    #     Key('PID').eq('p3')
-    # )
-    # items3 = response['Items']
-    # print(items3)
-    #
-    # response = table.query(
-    #     KeyConditionExpression=
-    #     Key('PID').eq('p4')
-    # )
-    # items4 = response['Items']
-    # print(items4)
-    #
-    # response = table.query(
-    #     KeyConditionExpression=
-    #     Key('PID').eq('p5')
-    # )
-    # items5 = response['Items']
-    # print(items5)
-    #
 
     return {
         # returns all items stored in response
