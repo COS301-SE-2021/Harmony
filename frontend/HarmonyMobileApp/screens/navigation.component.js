b05e4e4414d489d1e2364502d99f321399a417fa
Tiashka <seetaltiashka@gmail.com>
2021-06-28 17:14:03 +0200

    resolved merge conflicts

---
+++ frontend/HarmonyMobileApp/screens/navigation.component.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Layout,
+  Text,
+  Button,
+  Icon,
+} from "@ui-kitten/components";
+
+import HomeScreen from "./HomeScreen.js";
+import ViewFavouritesScreen from "./ViewFavouritesScreen.js";
+import ViewAllPairingsScreen from "./ViewAllPairingsScreen.js";
+import SettingsScreen from "./SettingsScreen.js";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const HomeIcon = (props) => <Icon {...props} name="home-outline" />;
+const TrendingIcon = (props) => <Icon {...props} name="trending-up-outline" />;
+const CameraIcon = (props) => (
+  <Icon {...props} name="camera-outline" width="32" height="32" />
+);
+const HeartIcon = (props) => <Icon {...props} name="heart-outline" />;
+const SettingsIcon = (props) => <Icon {...props} name="settings-2-outline" />;
+
+const CameraScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text category="h1">Camera</Text>
+  </Layout>
+);
+
+const BottomTabBar = ({ navigation, state }) => (
+  <BottomNavigation
+    selectedIndex={state.index}
+    onSelect={(index) => navigation.navigate(state.routeNames[index])}
+  >
+    <BottomNavigationTab icon={HomeIcon} />
+    <BottomNavigationTab icon={TrendingIcon} />
+    <BottomNavigationTab icon={CameraIcon} />
+    <BottomNavigationTab icon={HeartIcon} />
+    <BottomNavigationTab icon={SettingsIcon} />
+  </BottomNavigation>
+);
+
+const TabNavigator = () => (
+  <Navigator tabBar={(props) => <BottomTabBar {...props} />}>
+    <Screen name="Home" component={HomeScreen} />
+    <Screen name="Popular" component={ViewAllPairingsScreen} />
+    <Screen name="Camera" component={CameraScreen} />
+    <Screen name="Favourite" component={ViewFavouritesScreen} />
+    <Screen name="Settings" component={SettingsScreen} />
+  </Navigator>
+);
+
+export const AppNavigator = () => (
+  <NavigationContainer>
+    <TabNavigator />
+  </NavigationContainer>
+);
