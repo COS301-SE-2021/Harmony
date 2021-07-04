e31c7ec8b63a625838e57eef221de09bd78f9252
Jd Perumal <u18219285@tuks.co.za>
2021-07-04 14:55:31 +0200

    Implemented basic image and title layout
    Implemented Image and title components, no styling

--- frontend/HarmonyMobileApp/screens/PairingResultsScreen.js
+++ frontend/HarmonyMobileApp/screens/PairingResultsScreen.js
@@ -1,14 +1,60 @@
 import React, { useEffect, useState } from "react";
 
-import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  StatusBar,
+  Image,
+  Dimensions,
+  Platform,
+} from "react-native";
 import { Layout, Text } from "@ui-kitten/components";
+import {
+  ImageHeaderScrollView,
+  TriggeringView,
+} from "react-native-image-header-scroll-view";
+
+const MIN_HEIGHT = Platform.OS === "ios" ? 90 : 55;
+const MAX_HEIGHT = 350;
 
 const PairingResultsScreen = (props) => {
   return (
-    <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text category="h1">Pairing results</Text>
-    </Layout>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <ImageHeaderScrollView
+        maxHeight={MAX_HEIGHT}
+        minHeight={MIN_HEIGHT}
+        renderHeader={() => (
+          <Image
+            source={{ uri: "https://reactnative.dev/docs/assets/p_cat2.png" }}
+            style={styles.image}
+          />
+        )}
+      >
+        <TriggeringView>
+          <View>
+            <Text style={styles.title}>FOODNAME</Text>
+          </View>
+        </TriggeringView>
+      </ImageHeaderScrollView>
+    </View>
   );
 };
 
 export default PairingResultsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    height: MAX_HEIGHT,
+    width: Dimensions.get("window").width,
+    alignSelf: "stretch",
+    resizeMode: "cover",
+  },
+  title: {
+    fontSize: 20,
+  },
+});
