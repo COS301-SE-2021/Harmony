5a339e35cb27633737687e01820ad5fc9e601e8d
Jd Perumal <u18219285@tuks.co.za>
2021-07-04 16:52:24 +0200

    Implemented basic title, description and tags
    Created mock json data to simplify the development process while waiting for the real data.
    Implemented basic UI: title, description and tags with basic styling

--- frontend/HarmonyMobileApp/screens/PairingResultsScreen.js
+++ frontend/HarmonyMobileApp/screens/PairingResultsScreen.js
@@ -14,9 +14,23 @@ import {
   ImageHeaderScrollView,
   TriggeringView,
 } from "react-native-image-header-scroll-view";
+import { FontAwesome } from "@expo/vector-icons";
 
 const MIN_HEIGHT = Platform.OS === "ios" ? 90 : 55;
 const MAX_HEIGHT = 350;
+
+const response = {
+  statusCode: 200,
+  data: {
+    imageURI:
+      "https://www.eatout.co.za/wp-content/uploads/2014/11/koeksuster-recipe-20Mar13-043451.jpg",
+    foodItem: "Koeksister",
+    foodDesc:
+      "A koeksister also spelled koesister is a traditional Afrikaner confectionery made of fried dough infused in syrup or honey. There is also a Cape Malay version of the dish, which is a fried ball of dough that is rolled in desiccated coconut. ",
+    location: "Pretoria",
+    categories: ["Desert", "Sweet", "Snack"],
+  },
+};
 
 const PairingResultsScreen = (props) => {
   return (
@@ -27,16 +41,31 @@ const PairingResultsScreen = (props) => {
         minHeight={MIN_HEIGHT}
         renderHeader={() => (
           <Image
-            source={{ uri: "https://reactnative.dev/docs/assets/p_cat2.png" }}
+            source={{ uri: response.data.imageURI }}
             style={styles.image}
           />
         )}
       >
-        <TriggeringView>
+        <TriggeringView style={styles.section}>
           <View>
-            <Text style={styles.title}>FOODNAME</Text>
+            <Text style={styles.title}>{response.data.foodItem}</Text>
           </View>
         </TriggeringView>
+
+        <View style={[styles.section, styles.sectionLarge]}>
+          <Text style={styles.sectionContent}>{response.data.foodDesc}</Text>
+        </View>
+
+        <View style={styles.section}>
+          <View style={styles.categories}>
+            {response.data.categories.map((category, index) => (
+              <View style={styles.categoryContainer} key={index}>
+                <FontAwesome name="tag" size={16} color="#fff" />
+                <Text style={styles.category}>{category}</Text>
+              </View>
+            ))}
+          </View>
+        </View>
       </ImageHeaderScrollView>
     </View>
   );
@@ -56,5 +85,41 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
+  },
+  name: {
+    fontWeight: "bold",
+  },
+  section: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+    backgroundColor: "white",
+  },
+
+  sectionContent: {
+    fontSize: 16,
+    textAlign: "justify",
+  },
+  sectionLarge: {
+    minHeight: 300,
+  },
+  categories: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    flexWrap: "wrap",
+  },
+  categoryContainer: {
+    flexDirection: "row",
+    backgroundColor: "#FF6347",
+    borderRadius: 20,
+    margin: 10,
+    padding: 10,
+    paddingHorizontal: 15,
+  },
+  category: {
+    fontSize: 14,
+    color: "#fff",
+    marginLeft: 10,
   },
 });
