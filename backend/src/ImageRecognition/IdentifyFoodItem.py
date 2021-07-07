bc8e64a51fe5093cd66fd7cbf33f62478411c034
Arshad Sacoor <arshadsacoor@me.com>
2021-07-07 17:43:03 +0200

    Updated Identify FoodItem

--- backend/src/ImageRecognition/IdentifyFoodItem.py
+++ backend/src/ImageRecognition/IdentifyFoodItem.py
@@ -8,16 +8,24 @@ def identify_food_item(event, context):
     body = {
         "Url": "https://thestayathomechef.com/wp-content/uploads/2016/06/The-Most-Amazing-Chocolate-Cake-Square-1.jpg"}
     headers = {
-        'Prediction-Key': '{Prediction-Key}',
+        'Prediction-Key': '606bb8fd69394bc2bf192a8453995342',
         'Content-Type': 'application/json'
     }
 
     response = requests.post(url, headers=headers, data=json.dumps(body))
-
-    # load the json data
     data = response.json()
-    a = data['predictions'][0]['tagName']
-    b = data['predictions'][0]['probability']
-    print("Harmony has predicted a", a, " with a probability of", b)
+    keyVal = 'predictions'
 
-    return a + b
+    if keyVal in data:
+        a = data['predictions'][0]['tagName']
+        b = data['predictions'][0]['probability']
+        #print("Harmony has predicted a", a, " with a probability of", b)
+        return a
+    else:
+        #print("Predictions is not found in JSON data")
+        a = "An error has occurred"
+        b = data
+        #print(a,b)
+        return a
+
+
