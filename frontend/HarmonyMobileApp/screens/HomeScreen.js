e07d612333ca85b20a1011cb131d187c74513730
Tiashka <seetaltiashka@gmail.com>
2021-07-10 21:32:29 +0200

    Update HomeScreen.js
    updated tags

--- frontend/HarmonyMobileApp/screens/HomeScreen.js
+++ frontend/HarmonyMobileApp/screens/HomeScreen.js
@@ -12,11 +12,73 @@ import {
   Picker
 } from "react-native";
 import styles from "../styles";
-import { Feather, SimpleLineIcons } from '@expo/vector-icons';
+import {
+  Feather, SimpleLineIcons, FontAwesome,
+} from '@expo/vector-icons';
 import * as eva from '@eva-design/eva';
 import { default as theme } from '../theme.json';
 import { ApplicationProvider, Layout, Divider, Card, Text } from '@ui-kitten/components';
 import { useIsFocused } from "@react-navigation/native";
+
+const response = {
+  statusCode: 200,
+  data: {
+    imageURI:
+      "https://www.eatout.co.za/wp-content/uploads/2014/11/koeksuster-recipe-20Mar13-043451.jpg",
+    foodName: "Koeksister",
+    foodDesc:
+      "A koeksister also spelled koesister is a traditional Afrikaner confectionery made of fried dough infused in syrup or honey. There is also a Cape Malay version of the dish, which is a fried ball of dough that is rolled in desiccated coconut. ",
+    location: "Pretoria",
+    tags: ["Dessert", "Sweet", "Snack", "Warm", "Donut", "Baked"],
+    recommendedDrink: {
+      id: "99",
+      drinkName: "Tea",
+      drinkDesc:
+        "Tea is an aromatic beverage prepared by pouring hot or boiling water over cured or fresh leaves of Camellia sinensis, an evergreen shrub native to China and East Asia. ",
+      imageURI:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/tea-cup-bag-high-res-stock-photography-1570544677.jpg?resize=768:*",
+      tags: ["Hot", "Sweet", "Healthy", "Gluten-Free"],
+    },
+    drinkPairings: [
+      {
+        id: "1",
+        drinkName: "Espresso",
+        drinkDesc:
+          "Espresso is a coffee-brewing method of Italian origin, in which a small amount of nearly boiling water is forced under 9–10 bars of pressure through finely-ground coffee beans.",
+        imageURI:
+          "https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/beveragedaily.com/news/r-d/aussie-barista-helps-scientists-to-brew-the-perfect-espresso/10795433-1-eng-GB/Aussie-barista-helps-scientists-to-brew-the-perfect-espresso_wrbm_large.jpg",
+        tags: ["Hot", "Sweet", "Healthy", "Gluten-Free"],
+      },
+      {
+        id: "2",
+        drinkName: "Iced coffee",
+        drinkDesc:
+          "Iced coffee is a coffee beverage served cold. It may be prepared either by brewing coffee in the normal way and then serving it over ice or in cold milk, or by brewing the coffee cold.",
+        imageURI:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Blue_Bottle%2C_Kyoto_Style_Ice_Coffee_%285909775445%29.jpg/600px-Blue_Bottle%2C_Kyoto_Style_Ice_Coffee_%285909775445%29.jpg",
+        tags: ["Hot", "Sweet", "Healthy", "Gluten-Free"],
+      },
+      {
+        id: "3",
+        drinkName: "Iced coffee",
+        drinkDesc:
+          "Iced coffee is a coffee beverage served cold. It may be prepared either by brewing coffee in the normal way and then serving it over ice or in cold milk, or by brewing the coffee cold.",
+        imageURI:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Blue_Bottle%2C_Kyoto_Style_Ice_Coffee_%285909775445%29.jpg/600px-Blue_Bottle%2C_Kyoto_Style_Ice_Coffee_%285909775445%29.jpg",
+        tags: ["Hot", "Sweet", "Healthy", "Gluten-Free"],
+      },
+      {
+        id: "4",
+        drinkName: "Iced coffee",
+        drinkDesc:
+          "Iced coffee is a coffee beverage served cold. It may be prepared either by brewing coffee in the normal way and then serving it over ice or in cold milk, or by brewing the coffee cold.",
+        imageURI:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Blue_Bottle%2C_Kyoto_Style_Ice_Coffee_%285909775445%29.jpg/600px-Blue_Bottle%2C_Kyoto_Style_Ice_Coffee_%285909775445%29.jpg",
+        tags: ["Hot", "Sweet", "Healthy", "Gluten-Free"],
+      },
+    ],
+  },
+};
 
 const HomeScreen = (props) => {
   const viewPairingURL =
@@ -148,13 +210,7 @@ const HomeScreen = (props) => {
                       </View>
                     </View>
 
-                    <View style={styles.tagBar}>
-                      <Text style={styles.tag}><Feather name="tag" size={15} color="white" />Salty</Text>
-                      <Text style={styles.tag}><Feather name="tag" size={15} color="white" />Dessert</Text>
-                    </View><View style={styles.tagBar}>
-                      <Text style={styles.tag}><Feather name="tag" size={15} color="white" />Medium Sized</Text>
-                      <Text style={styles.tag}><Feather name="tag" size={15} color="white" />Contains Dairy</Text>
-                    </View>
+                    <TagBar />
                     <View style={styles.locationBar}>
                       <SimpleLineIcons name="location-pin" size={25} color="white" />
                       <Text style={styles.TextSmallWhite}>Prospect Street, Pretoria, Gauteng </Text>
@@ -188,6 +244,31 @@ const HomeScreen = (props) => {
     </ApplicationProvider>
   );
 };
+
+const TagBar = () => (
+  <View style={styles.tagsSection}>
+    <ScrollView
+      contentContainerStyle={{
+        flexDirection: "row",
+        flexWrap: "wrap",
+      }}
+      showsHorizontalScrollIndicator={false}
+      horizontal={true}
+    >
+      <View style={styles.rowContainer}>
+        {response.data.tags.map((tag, index) => (
+          <View style={styles.tagContainer} key={index}>
+            <FontAwesome name="tag" size={16} color="#fff" />
+            {/* Keeping outlined icons just incase we want to change to them for consistency overall */}
+            {/* The filled icons look better in this case though */}
+            {/* <Feather name="tag" size={16} color="#fff" /> */}
+            <Text style={styles.tagText}>{tag}</Text>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
+  </View>
+);
 const personalStyles = StyleSheet.create({
 
   text: {
