dab65f992b68d74e760d9192ababe34ff2103995
Keval157 <u18012559@tuks.co.za>
2021-07-23 14:32:59 +0200

    Completed the update aspect for this function with hard coded input.

--- backend/src/Pairings/ViewPairings/Voting.py
+++ backend/src/Pairings/ViewPairings/Voting.py
@@ -1,14 +1,36 @@
 import json
 import boto3
 
-dynamodb = boto3.resource('dynamodb')
 
-# use the DynamoDB object to select our table
-table_name = 'Pairings'
-table = dynamodb.Table(table_name)
-
-
+# receives ID of the pairing
 def vote(event, context):
-    print("Test")
+    dynamodb = boto3.resource('dynamodb')
 
-    return 0
+    # use the DynamoDB object to select our table
+    table_name = 'Pairings'
+    table = dynamodb.Table(table_name)
+
+    print("Test the code")
+    type = 'Upvotes'  # or Down and must be passed in
+    id = '60dyrtjoucir6556'
+
+    # response = table.get_item(Key={'PID': id})
+    numvotes = 6  # get from DB using ID of pairing based on whether its up or down
+    if type == 'Upvotes':
+        numvotes = numvotes + 1  # increment
+
+    response = table.update_item(
+        TableName=table_name,
+        Key={
+            'PID': id
+        },
+        ExpressionAttributeNames={'#V': type},
+        ExpressionAttributeValues={':v': numvotes},
+        UpdateExpression='SET #V = :v',
+        ReturnValues="UPDATED_NEW"
+
+    )
+
+    # write numvotes back to table
+
+    return response
