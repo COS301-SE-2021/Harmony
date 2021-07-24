1d72aafd413b49a043a65696427c8c0e71066ca0
Keval157 <u18012559@tuks.co.za>
2021-07-24 12:18:39 +0200

    Implemented the read in order to work with actual data. Added implementation to work with actual data and made adjustments to the logic

--- backend/src/Pairings/ViewPairings/Voting.py
+++ backend/src/Pairings/ViewPairings/Voting.py
@@ -1,8 +1,10 @@
 import json
 import boto3
+from botocore.exceptions import ClientError
 
 
-# receives ID of the pairing
+# receives ID of the pairing, whether it was upvote/downvote,
+# whether it was clicked/unclicked.
 def vote(event, context):
     dynamodb = boto3.resource('dynamodb')
 
@@ -11,13 +13,27 @@ def vote(event, context):
     table = dynamodb.Table(table_name)
 
     print("Test the code")
-    type = 'Upvotes'  # or Down and must be passed in
-    id = '60dyrtjoucir6556'
 
-    # response = table.get_item(Key={'PID': id})
-    numvotes = 6  # get from DB using ID of pairing based on whether its up or down
-    if type == 'Upvotes':
-        numvotes = numvotes + 1  # increment
+    type = 'Upvotes'  # or Down and must be passed in from frontend
+    id = '60dyrtjoucir6556'  # must be passed in from frontend
+    vote_type = 'Checked'
+
+    # gets the pairing that the vote was made to as well as its data.
+    try:
+        pairing_data = table.get_item(Key={'PID': id})
+        print(pairing_data)
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+
+    # extracts the number of votes from the pairing response string depending
+    # on the type, either: Upvotes/Downvotes
+    num_votes = pairing_data['Item'][type]
+    print(num_votes)
+
+    if vote_type == 'Checked':
+        num_votes = num_votes + 1
+    else:
+        num_votes = num_votes - 1
 
     response = table.update_item(
         TableName=table_name,
@@ -25,7 +41,7 @@ def vote(event, context):
             'PID': id
         },
         ExpressionAttributeNames={'#V': type},
-        ExpressionAttributeValues={':v': numvotes},
+        ExpressionAttributeValues={':v': num_votes},
         UpdateExpression='SET #V = :v',
         ReturnValues="UPDATED_NEW"
 
