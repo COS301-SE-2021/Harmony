f47dcdf0152c6229d7a7534bb4db3ef3a7768677
Jd Perumal <u18219285@tuks.co.za>
2021-07-25 13:51:47 +0200

    Created SignUpScreen

---
+++ frontend/HarmonyMobileApp/screens/SignUp.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Auth } from "aws-amplify";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import AppTextInput from "./AppTextInput";
+import AppButton from "./AppButton";
+
+export default function SignUp({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  async function signUp() {
+    try {
+      await Auth.signUp({ username, password, attributes: { email } });
+      console.log(" Sign-up Confirmed");
+      navigation.navigate("ConfirmSignUp");
+    } catch (error) {
+      console.log(" Error signing up...", error);
+    }
+  }
+
+  return (
+    <SafeAreaView style={styles.safeAreaContainer}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Create a new account</Text>
+        <AppTextInput
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+          leftIcon="account"
+          placeholder="Enter username"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+        />
+        <AppTextInput
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          leftIcon="lock"
+          placeholder="Enter password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry
+          textContentType="password"
+        />
+        <AppTextInput
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          leftIcon="email"
+          placeholder="Enter email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+        />
+        <AppButton title="Sign Up" onPress={signUp} />
+        <View style={styles.footerButtonContainer}>
+          <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
+            <Text style={styles.forgotPasswordButtonText}>
+              Already have an account? Sign In
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeAreaContainer: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    color: "#202020",
+    fontWeight: "500",
+    marginVertical: 15,
+  },
+  footerButtonContainer: {
+    marginVertical: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  forgotPasswordButtonText: {
+    color: "tomato",
+    fontSize: 18,
+    fontWeight: "600",
+  },
+});
