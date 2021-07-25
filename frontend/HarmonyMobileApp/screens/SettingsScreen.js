ff6820b0182856d12405ef452dfad5f6223304f9
Jd Perumal <u18219285@tuks.co.za>
2021-07-25 13:58:21 +0200

    Added SignOut functionality to SettingsScreen

--- frontend/HarmonyMobileApp/screens/SettingsScreen.js
+++ frontend/HarmonyMobileApp/screens/SettingsScreen.js
@@ -1,14 +1,44 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
-import { Layout, Text } from "@ui-kitten/components";
+// import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
+// import { Layout, Text } from "@ui-kitten/components";
 
-const SettingsScreen = (props) => {
+// const SettingsScreen = (props) => {
+//   return (
+//     <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//       <Text category="h1">Settings</Text>
+//     </Layout>
+//   );
+// };
+
+// export default SettingsScreen;
+
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { Auth } from "aws-amplify";
+
+export default function SettingsScreen({ updateAuthState }) {
+  async function signOut() {
+    try {
+      await Auth.signOut();
+      updateAuthState("loggedOut");
+    } catch (error) {
+      console.log("Error signing out: ", error);
+    }
+  }
+
   return (
-    <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text category="h1">Settings</Text>
-    </Layout>
+    <View style={styles.container}>
+      <Text> + </Text>
+      <Button title="Sign Out" color="tomato" onPress={signOut} />
+    </View>
   );
-};
+}
 
-export default SettingsScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20,
+  },
+});
