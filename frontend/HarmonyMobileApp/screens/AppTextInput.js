9fed37a38a0b4cb73f125f63ead0f2c8337ac86f
Jd Perumal <u18219285@tuks.co.za>
2021-07-25 13:42:28 +0200

    Created reusable button and textInput components
    These components will be used throughout the login/signUp flow/screens

---
+++ frontend/HarmonyMobileApp/screens/AppTextInput.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, StyleSheet, TextInput } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+export default function AppTextInput({ leftIcon, ...otherProps }) {
+  return (
+    <View style={styles.container}>
+      {leftIcon && (
+        <MaterialCommunityIcons
+          name={leftIcon}
+          size={20}
+          color="#6e6869"
+          style={styles.icon}
+        />
+      )}
+      <TextInput
+        style={styles.input}
+        placeholderTextColor="#6e6869"
+        {...otherProps}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#f9f9f9",
+    borderRadius: 25,
+    flexDirection: "row",
+    padding: 15,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    width: "80%",
+    fontSize: 18,
+    color: "#101010",
+  },
+});
