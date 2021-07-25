523e609fa2a0e83590f6a1111c3da98e56ddd60f
Jd Perumal <u18219285@tuks.co.za>
2021-07-25 13:53:56 +0200

    Created ConfirmSignUpScreen

---
+++ frontend/HarmonyMobileApp/screens/ConfirmSignUp.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Auth } from "aws-amplify";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import AppTextInput from "./AppTextInput";
+import AppButton from "./AppButton";
+
+export default function ConfirmSignUp({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [authCode, setAuthCode] = useState("");
+
+  async function confirmSignUp() {
+    try {
+      await Auth.confirmSignUp(username, authCode);
+      console.log(" Code confirmed");
+      navigation.navigate("SignIn");
+    } catch (error) {
+      console.log(
+        " Verification code does not match. Please enter a valid verification code.",
+        error.code
+      );
+    }
+  }
+
+  return (
+    <SafeAreaView style={styles.safeAreaContainer}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Confirm Sign Up</Text>
+        <AppTextInput
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+          leftIcon="account"
+          placeholder="Enter username"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+        />
+        <AppTextInput
+          value={authCode}
+          onChangeText={(text) => setAuthCode(text)}
+          leftIcon="numeric"
+          placeholder="Enter verification code"
+          keyboardType="numeric"
+        />
+        <AppButton title="Confirm Sign Up" onPress={confirmSignUp} />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeAreaContainer: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    color: "#202020",
+    fontWeight: "500",
+    marginVertical: 15,
+  },
+});
