1417fcaf37498d1cf94a91a234de044dd1d2073c
MohammedCarrim <u18080813@tuks.co.za>
2021-07-28 23:42:51 +0200

    added new implementation to ViewPairings.py, added search functionality for user specific upvotes,downvotes, favourites

--- backend/src/Pairings/ViewPairings/ViewPairings.py
+++ backend/src/Pairings/ViewPairings/ViewPairings.py
@@ -4,17 +4,25 @@ from boto3.dynamodb.conditions import Key
 
 client = boto3.resource('dynamodb')
 table = client.Table('Pairings')
-
+usertable = client.Table('Users')
 """This function will take in the UID of the user, as a json event. This function returns 
  the data in the pairings table, sorted by Total votes."""
 
 
 def View_Pairings(event, context):
+    uid = event['UID']
+    userResponse = get_user_response(uid)
+
     # add entire table to allresponse
     allresponse = table.scan()
     # edit the response to only show items
     response = allresponse['Items']
-    item1 = response[0]
+
+    userUpvotes = userResponse['UserUpvoted']
+    userDownvotes = userResponse['UserDownvoted']
+    userFavourites = userResponse['FavouritePairings']
+
+    response = add_userdata(response, userFavourites, userUpvotes, userDownvotes)
     # iterate through response and append total votes
     for i in response:
         totalvotes = i['Upvotes'] + i['Downvotes']
@@ -32,3 +40,19 @@ def View_Pairings(event, context):
 # this functions returns the json value we will ant to sort by
 def sortFunction(value):
     return value["TotalVotes"]
+
+
+def get_user_response(uid):
+    response = usertable.get_item(
+        Key={'UID': uid}
+    )
+
+    return response['Item']
+
+
+def add_userdata(response, userFavourites, userUpvotes, userDownvotes):
+    for i in response:
+        for favs in userFavourites:
+            if favs == i["PID"]:
+                print(favs)
+    return response
