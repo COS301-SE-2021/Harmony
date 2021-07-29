d373d5b3e4e7ade9996489670f6104285998ddfb
Jd Perumal <u18219285@tuks.co.za>
2021-07-30 01:56:22 +0200

    Created basic custom alert

--- frontend/HarmonyMobileApp/screens/SignIn.js
+++ frontend/HarmonyMobileApp/screens/SignIn.js
@@ -6,6 +6,7 @@ import {
   StatusBar,
   Platform,
   Alert,
+  TouchableOpacity,
 } from "react-native";
 import * as yup from "yup";
 import { Formik } from "formik";
@@ -21,9 +22,15 @@ import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view
 import { SocialIcon } from "react-native-elements";
 import * as Animatable from "react-native-animatable";
 import { showMessage, hideMessage } from "react-native-flash-message";
-
+import Modal from "react-native-modal";
+import {
+  FontAwesome,
+  MaterialIcons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 export default function SignIn({ navigation, updateAuthState }) {
   const [isLoading, setLoading] = useState(false);
+  const [isModalVisible, setModalVisible] = useState(false);
 
   async function signIn(values) {
     try {
@@ -39,15 +46,81 @@ export default function SignIn({ navigation, updateAuthState }) {
     } catch (error) {
       console.log(" Error signing in...", error);
       // Alert.alert("Error", error.message);
-      showMessage({
-        message: "Error",
-        description: error.message,
-        type: "danger",
-      });
+      setModalVisible(true);
+      // showMessage({
+      //   message: "Error",
+      //   description: error.message,
+      //   type: "danger",
+      // });
 
       setLoading(false);
     }
   }
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  const FeedbackModal = () => (
+    <Modal
+      isVisible={isModalVisible}
+      onBackButtonPress={toggleModal}
+      onBackdropPress={toggleModal}
+      animationIn={"slideInRight"}
+      animationOut={"slideOutRight"}
+      swipeDirection={["up", "left", "right", "down"]}
+      onSwipeComplete={toggleModal}
+    >
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <TouchableOpacity
+            style={[styles.closeButton]}
+            onPress={() => setModalVisible(!isModalVisible)}
+          >
+            <MaterialCommunityIcons
+              name="close-circle-outline"
+              size={30}
+              color="black"
+            />
+          </TouchableOpacity>
+          <View style={[styles.modalHeaderSection]}>
+            <Text style={styles.title}>Help us improve </Text>
+          </View>
+          <Text style={styles.modalText}>
+            Is the food correctly identified?
+          </Text>
+          <View style={styles.rowContainer}>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonIncorrect]}
+              onPress={() => setModalVisible(!isModalVisible)}
+            >
+              {/* Keeping outlined icons just incase we want to change to them for consistency overall */}
+              {/* The filled icons look better in this case though */}
+              {/* <MaterialIcons
+                    name="thumb-down-off-alt"
+                    size={40}
+                    color="white"
+                  /> */}
+              <MaterialIcons name="thumb-down" size={40} color="white" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonCorrect]}
+              onPress={() => setModalVisible(!isModalVisible)}
+            >
+              {/* Keeping outlined icons just incase we want to change to them for consistency overall */}
+              {/* The filled icons look better in this case though */}
+              {/* <MaterialIcons
+                    name="thumb-up-off-alt"
+                    size={40}
+                    color="white"
+                  /> */}
+              <MaterialIcons name="thumb-up" size={40} color="white" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
 
   return (
     <Formik
@@ -156,6 +229,8 @@ export default function SignIn({ navigation, updateAuthState }) {
               </View>
             </Animatable.View>
           </View>
+          <FeedbackModal />
+
           {isLoading === true && <AppLoadingIcon />}
         </KeyboardAwareScrollView>
       )}
@@ -223,5 +298,52 @@ const styles = StyleSheet.create({
     // color: "#00ffff",
     fontSize: 19,
     fontWeight: "600",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 4,
+    margin: 5,
+  },
+  buttonCorrect: {
+    backgroundColor: "#56a211",
+  },
+  buttonIncorrect: {
+    backgroundColor: "#e9430f",
+  },
+  closeButton: {
+    position: "absolute",
+    right: 10,
+    top: 5,
+  },
+  modalHeaderSection: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+    backgroundColor: "white",
+  },
+  modalText: {
+    paddingVertical: 10,
+    fontSize: 20,
+    textAlign: "center",
   },
 });
