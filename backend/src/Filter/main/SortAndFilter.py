767d770a54f492fc25fb755444ccfb32d76d2878
MohammedCarrim <u18080813@tuks.co.za>
2021-07-29 23:43:54 +0200

    implemented include user data for pairings in SortAndFilter.py

--- backend/src/Filter/main/SortAndFilter.py
+++ backend/src/Filter/main/SortAndFilter.py
@@ -5,17 +5,18 @@ from boto3.dynamodb.conditions import Key
 table_name = 'Pairings'
 User_table = 'Users'
 
-
 client = boto3.resource('dynamodb')
 
 table = client.Table(table_name)
-user_table = client.Table(User_table)
+usertable = client.Table(User_table)
 
 """This function will take in the UID of the user, as a json event. This function returns 
  the data in the pairings table"""
 
 
 def sort_and_filter(event, context):
+    uid = event['UID']
+    userResponse = get_user_response(uid)
     # add entire table to allresponse
     allresponse = table.scan()
     # edit the response to only show items
@@ -28,17 +29,13 @@ def sort_and_filter(event, context):
     elif event['Sort'] == 'Trending':
         sortedResponse = sortbytrending(response)
 
-
-
+    sortedResponse = add_userdata(sortedResponse, userResponse)
 
     return {
         # returns all items stored in response
         "StatusCode": 200,
         "Data": sortedResponse
     }
-
-
-
 
 
 def sortbynew(response):
@@ -71,12 +68,44 @@ def filtertags(a):
 def filterdistance(a):
     return 0
 
+
 # this functions returns the json value we will want to sort by
 def upvotes_function(value):
     return value["Upvotes"]
+
 
 # this functions returns the json value we will want to sort by
 def totalvotes_function(value):
     return value["TotalVotes"]
 
 
+def get_user_response(uid):
+    response = usertable.get_item(
+        Key={'UID': uid}
+    )
+
+    return response['Item']
+
+
+def add_userdata(pairingresponse, user_response):
+    userUpvotes = user_response['UserUpvoted']
+    userDownvotes = user_response['UserDownvoted']
+    userFavourites = user_response['FavouritePairings']
+
+    for i in pairingresponse:
+        i['isUpvoted'] = "False"
+        i['isDownvoted'] = "False"
+        i['isFavourited'] = "False"
+        for favs in userFavourites:
+            if favs == i["PID"]:
+                i['isFavourited'] = "True"
+
+        for up in userUpvotes:
+            if up == i["PID"]:
+                i['isUpvoted'] = "True"
+
+        for down in userDownvotes:
+            if down == i["PID"]:
+                i['isDownvoted'] = "True"
+
+    return pairingresponse
