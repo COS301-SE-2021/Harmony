4621aee10b8249ebf5f0e9e098c99881de1c8173
Keval157 <u18012559@tuks.co.za>
2021-07-29 20:15:03 +0200

    Made template for filter and sort functionality.

--- backend/src/Filter/main/SortAndFilter.py
+++ backend/src/Filter/main/SortAndFilter.py
@@ -2,8 +2,14 @@ import json
 import boto3
 from boto3.dynamodb.conditions import Key
 
+table_name = 'Pairings'
+User_table = 'Users'
+
+
 client = boto3.resource('dynamodb')
-table = client.Table('Pairings')
+
+table = client.Table(table_name)
+user_table = client.Table(User_table)
 
 """This function will take in the UID of the user, as a json event. This function returns 
  the data in the pairings table"""
@@ -16,8 +22,11 @@ def sort_and_filter(event, context):
     response = allresponse['Items']
     item1 = response[0]
 
+    if event['sort'] == 'New':
+        sortedResponse = sortbynew(response)
 
-    sortedResponse = sorted(response, key=sortFunction, reverse=True)
+
+
     return {
         # returns all items stored in response
         "StatusCode": 200,
@@ -28,3 +37,27 @@ def sort_and_filter(event, context):
 # this functions returns the json value we will want to sort by
 def sortFunction(value):
     return value["Upvotes"]
+
+
+def sortbynew(response):
+    return 0
+
+
+def sortbycontroversial(a):
+    return 0
+
+
+def sortbybest(a):
+    return 0
+
+
+def sortbytrending(a):
+    return 0
+
+
+def filtertags(a):
+    return 0
+
+
+def filterdistance(a):
+    return 0
