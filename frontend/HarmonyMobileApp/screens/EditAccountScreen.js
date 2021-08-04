9000aa657776eb3f80abde0649c70b6c53c4b3f0
Jd Perumal <u18219285@tuks.co.za>
2021-08-04 23:19:07 +0200

    Completed EditAccountDetailsScreen styling

--- frontend/HarmonyMobileApp/screens/EditAccountScreen.js
+++ frontend/HarmonyMobileApp/screens/EditAccountScreen.js
@@ -1,13 +1,87 @@
 import React from "react";
-import { View, Text, SafeAreaView, Image } from "react-native";
-import styles from "../styles";
-
-const EditAccountScreen = (props) => {
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+  Alert,
+} from "react-native";
+export default function EditAccountScreen({ navigation }) {
+  const RightIcon = () => <Text style={styles.rightIcon}>EDIT</Text>;
   return (
-    <View>
-      <Text> EditAccountScreen </Text>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Edit Account Details")}
+      >
+        <View style={styles.list}>
+          <View>
+            <Text style={[styles.listText, styles.placeholderText]}>
+              Username
+            </Text>
+            <Text style={styles.listText}>Example1234</Text>
+          </View>
+          <RightIcon />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Edit Account Details")}
+      >
+        <View style={styles.list}>
+          <View>
+            <Text style={[styles.listText, styles.placeholderText]}>Email</Text>
+            <Text style={styles.listText}>example@gmail.com</Text>
+          </View>
+          <RightIcon />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Edit Account Details")}
+      >
+        <View style={styles.list}>
+          <View>
+            <Text style={[styles.listText, styles.placeholderText]}>
+              Password
+            </Text>
+            <Text style={styles.listText}>********</Text>
+          </View>
+          <RightIcon />
+        </View>
+      </TouchableOpacity>
     </View>
   );
-};
-
-export default EditAccountScreen;
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ecf0f1",
+    padding: 3,
+  },
+  list: {
+    padding: 20,
+    backgroundColor: "white",
+    borderRadius: 5,
+    marginTop: 1,
+    flexDirection: "row",
+    // justifyContent: "center",
+  },
+  listText: {
+    fontSize: 18,
+    // alignSelf: "center",
+  },
+  placeholderText: {
+    color: "#888",
+    marginBottom: 10,
+  },
+  rightIcon: {
+    flex: 1,
+    justifyContent: "flex-end",
+    position: "absolute",
+    right: "10%",
+    top: "75%",
+    fontSize: 18,
+    color: "#118AB2",
+  },
+});
