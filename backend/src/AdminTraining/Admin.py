8a074e414af93f98cf0ce8fda608553e8190ff48
Arshad Sacoor <arshadsacoor@me.com>
2021-08-10 13:50:21 +0200

    Update GetTags Function

--- backend/src/AdminTraining/Admin.py
+++ backend/src/AdminTraining/Admin.py
@@ -1,56 +1,18 @@
-from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
-from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
-from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateBatch, ImageFileCreateEntry, Region
-from msrest.authentication import ApiKeyCredentials
-import os, time, uuid
+import requests
+import json
 
-# Replace with valid values
-ENDPOINT = "PASTE_YOUR_CUSTOM_VISION_TRAINING_ENDPOINT_HERE"
-training_key = "PASTE_YOUR_CUSTOM_VISION_TRAINING_SUBSCRIPTION_KEY_HERE"
-prediction_key = "PASTE_YOUR_CUSTOM_VISION_PREDICTION_SUBSCRIPTION_KEY_HERE"
-prediction_resource_id = "PASTE_YOUR_CUSTOM_VISION_PREDICTION_RESOURCE_ID_HERE"
+url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/tags?iterationId=3"
 
-credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
-trainer = CustomVisionTrainingClient(ENDPOINT, credentials)
-prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
-predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
+body = {}
+headers = {
+    'Training-Key': '660f5ff2178347299730d1dbee11d350',
+    'Content-Type': 'application/json'
 
-# Make two tags in the new project
-hemlock_tag = trainer.create_tag(project.id, "Hemlock")
-cherry_tag = trainer.create_tag(project.id, "Japanese Cherry")
+}
 
-#To add the sample images to the project, insert the following code after the tag creation. This code uploads each image with its corresponding tag. You can upload up to 64 images in a single batch.
-base_image_location = os.path.join (os.path.dirname(__file__), "Images")
+response = requests.get(url, headers=headers)
+# print(response.content)
+# load the json data
+data = response.json()
 
-print("Adding images...")
-
-image_list = []
-
-for image_num in range(1, 11):
-    file_name = "hemlock_{}.jpg".format(image_num)
-    with open(os.path.join (base_image_location, "Hemlock", file_name), "rb") as image_contents:
-        image_list.append(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[hemlock_tag.id]))
-
-for image_num in range(1, 11):
-    file_name = "japanese_cherry_{}.jpg".format(image_num)
-    with open(os.path.join (base_image_location, "Japanese_Cherry", file_name), "rb") as image_contents:
-        image_list.append(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[cherry_tag.id]))
-
-upload_result = trainer.create_images_from_files(project.id, ImageFileCreateBatch(images=image_list))
-if not upload_result.is_batch_successful:
-    print("Image batch upload failed.")
-    for image in upload_result.images:
-        print("Image status: ", image.status)
-    exit(-1)
-
-print ("Training...")
-iteration = trainer.train_project(project.id)
-while (iteration.status != "Completed"):
-    iteration = trainer.get_iteration(project.id, iteration.id)
-    print ("Training status: " + iteration.status)
-    print ("Waiting 10 seconds...")
-    time.sleep(10)
-
-    # The iteration is now trained. Publish it to the project endpoint
-    trainer.publish_iteration(project.id, iteration.id, publish_iteration_name, prediction_resource_id)
-    print("Done!")
+print(data)
