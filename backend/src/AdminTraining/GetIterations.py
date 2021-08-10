3e484a9f63fade2f8aabb77fcef927a9fd964efe
Arshad Sacoor <arshadsacoor@me.com>
2021-08-10 13:53:02 +0200

    Updated Get Iterations function

---
+++ backend/src/AdminTraining/GetIterations.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/iterations"
+
+body = {}
+headers = {
+    'Training-Key': '660f5ff2178347299730d1dbee11d350',
+    'Content-Type': 'application/json'
+
+}
+
+response = requests.get(url, headers=headers)
+# print(response.content)
+# load the json data
+data = response.json()
+
+print(data)
