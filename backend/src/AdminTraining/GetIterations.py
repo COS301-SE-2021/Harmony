cf52b4d4a75df20add73d5852a170139606a7788
Keval157 <u18012559@tuks.co.za>
2021-08-11 13:19:33 +0200

    Put the GetIterations.py in a lambda function

--- backend/src/AdminTraining/GetIterations.py
+++ backend/src/AdminTraining/GetIterations.py
@@ -1,18 +1,21 @@
 import requests
 import json
+import boto3
 
-url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/iterations"
 
-body = {}
-headers = {
-    'Training-Key': '',
-    'Content-Type': 'application/json'
+def get_iterations(event, context):
+    url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/iterations"
 
-}
+    body = {}
+    headers = {
+        'Training-Key': '',
+        'Content-Type': 'application/json'
 
-response = requests.get(url, headers=headers)
-# print(response.content)
-# load the json data
-data = response.json()
+    }
 
-print(data)
+    response = requests.get(url, headers=headers)
+
+    # load the json data
+    data = response.json()
+    print(data)
+    return data
