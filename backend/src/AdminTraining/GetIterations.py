8ca77188f3e4dbf7c7e73ab0718ce7c5c15b2516
Arshad Sacoor <arshadsacoor@me.com>
2021-08-11 17:01:01 +0200

    Added Createtag.py file

--- backend/src/AdminTraining/GetIterations.py
+++ backend/src/AdminTraining/GetIterations.py
@@ -3,7 +3,6 @@ import json
 
 url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/iterations"
 
-body = {}
 headers = {
     'Training-Key': '',
     'Content-Type': 'application/json'
