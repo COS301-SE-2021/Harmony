ef92a701d2d52794e5b21883b918bad5448a0501
Keval157 <u18012559@tuks.co.za>
2021-08-12 12:03:43 +0200

    Added a lambda function to CreateTag.py

--- backend/src/AdminTraining/main/CreateTag.py
+++ backend/src/AdminTraining/main/CreateTag.py
@@ -1,18 +1,25 @@
 import requests
 import json
+import boto3
 
-#create tag : the tag name will come from the user
-tag = "tag_name"
-url_semi = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/tags?name="
-url = url_semi+tag
 
-headers = {
-    'Training-Key': '',
-    'Content-Type': 'application/json'
+def create_tags(event, context):
+    # create tag : the tag name will come from the user
+    #input
 
-}
+    tag = "tag_name"
+    url_semi = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/tags?name="
+    url = url_semi + tag
 
-response = requests.post(url, headers=headers, )
-data = response.json()
+    headers = {
+        'Training-Key': '',
+        'Content-Type': 'application/json'
 
-print(data)
+    }
+
+    response = requests.post(url, headers=headers, )
+
+    # load the json data
+    data = response.json()
+    print(data)
+    return data
