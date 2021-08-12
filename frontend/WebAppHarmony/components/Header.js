8d9d6ad727f9a8bbaaea64574e4bbe097673ab2a
Tiashka <seetaltiashka@gmail.com>
2021-08-12 15:44:22 +0200

    added a header

---
+++ frontend/WebAppHarmony/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../src/App.css';
+
+function Header() {
+    return (
+        <div id="Header">
+            <div id="adminWord"> <p>Admin</p></div>
+        </div>
+    );
+}
+
+export default Header;
