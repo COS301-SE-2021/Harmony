8dde8cf43d77d90799ca380a7c225e6bb4a1a814
Arshad Sacoor <arshadsacoor@me.com>
2021-08-14 18:57:16 +0200

    Updated App.js Connected Api to Table

--- frontend/WebAppHarmony/src/App.js
+++ frontend/WebAppHarmony/src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Table from "./TableContainer";
+import { SelectColumnFilter } from "./Filter";
+
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios("http://api.tvmaze.com/search/shows?q=girls")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const columns = [
+    {
+      Header: "Name",
+      accessor: "show.name",
+    },
+    {
+      Header: "Type",
+      accessor: "show.type",
+    },
+    {
+      Header: "Language",
+      accessor: "show.language",
+    },
+    {
+      Header: "Official Site",
+      accessor: "show.officialSite",
+      Cell: ({ cell: { value } }) =>
+        value ? <a href={value}>{value}</a> : "-",
+    },
+    {
+      Header: "Rating",
+      accessor: "show.rating.average",
+      Cell: ({ cell: { value } }) => value || "-",
+    },
+    {
+      Header: "Status",
+      accessor: "show.status",
+      Filter: SelectColumnFilter,
+      filter: "includes",
+    },
+    {
+      Header: "Premiered",
+      accessor: "show.premiered",
+      // disable the filter for particular column
+      disableFilters: true,
+      Cell: ({ cell: { value } }) => value || "-",
+    },
+    {
+      Header: "Time",
+      accessor: "show.schedule.time",
+      disableFilters: true,
+      Cell: ({ cell: { value } }) => value || "-",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        <center>React Table Demo</center>
+      </h1>
+      <Table columns={columns} data={data} />
     </div>
   );
 }
