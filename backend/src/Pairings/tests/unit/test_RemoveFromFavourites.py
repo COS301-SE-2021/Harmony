10d5583d1b17dc99eb29b2c225e7a6e38b38ccd9
MohammedCarrim <u18080813@tuks.co.za>
2021-08-16 00:56:14 +0200

    restructured files and removed unnecessary prints from AddToFavourites.py

--- backend/src/Pairings/tests/unit/test_RemoveFromFavourites.py
+++ backend/src/Pairings/tests/unit/test_RemoveFromFavourites.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from backend.src.Pairings.removeFromFavourites import RemoveFromFavourites
+from backend.src.Pairings.main import RemoveFromFavourites
 
 
 @pytest.fixture()
