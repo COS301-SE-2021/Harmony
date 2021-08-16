b9b556fab028d55d27a23c186c36c0741ea7c840
Tiashka <seetaltiashka@gmail.com>
2021-08-16 18:33:19 +0200

    added the two containers onto moderate pairing

--- frontend/WebAppHarmony/src/ModeratePairing.js
+++ frontend/WebAppHarmony/src/ModeratePairing.js
@@ -3,10 +3,36 @@ import './App.css';
 
 //user ages, demographics
 function ModeratePairing() {
-
+    const requestContainer = {
+        width: "20%",
+        height: 600,
+        padding: 15,
+        borderRadius: 15,
+        backgroundColor: "white",
+        float: "left",
+        // margin: "auto",
+        boxShadow: " 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+    };
+    const addItemContainer = {
+        width: "65%",
+        height: 600,
+        marginLeft: 100,
+        padding: 15,
+        borderRadius: 15,
+        float: "left",
+        backgroundColor: "white",
+        textAlign: "center",
+        // margin: "auto",
+        boxShadow: " 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+    };
     return (
-        <div style={{ flexDirection: "row", justifyContent: "space-around" }}>
-            <p>in moderate</p>
+        <div style={{ justifyContent: "center", display: "flex", flexDirection: "row", paddingTop: 25, backgroundColor: "#F3F3F3" }}>
+            <div style={requestContainer}>
+                <p>Users have requested the following items to be added to the database:</p>
+            </div>
+            <div style={addItemContainer}>
+                <p>Add an item to the database:</p>
+            </div>
         </div>
 
     );
