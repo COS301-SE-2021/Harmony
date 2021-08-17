b2bdd95696a5df8f7a2adc98bb6220f7b362aeb9
Tiashka <seetaltiashka@gmail.com>
2021-08-17 09:45:42 +0200

    added the mappings for the request item

--- frontend/WebAppHarmony/src/ModeratePairing.js
+++ frontend/WebAppHarmony/src/ModeratePairing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+// import { Formik } from 'formik';
 
 //user ages, demographics
 function ModeratePairing() {
@@ -10,6 +11,7 @@ function ModeratePairing() {
         borderRadius: 15,
         backgroundColor: "white",
         float: "left",
+        textAlign: "center",
         // margin: "auto",
         boxShadow: " 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
     };
@@ -25,10 +27,14 @@ function ModeratePairing() {
         // margin: "auto",
         boxShadow: " 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
     };
+
+    const requestedItems = ["Malva Pudding", "Cape Malay Curry", "Biltong", "Boerewors", "Amarula Don Pedro", "Melktert"];
     return (
         <div style={{ justifyContent: "center", display: "flex", flexDirection: "row", paddingTop: 25, backgroundColor: "#F3F3F3" }}>
             <div style={requestContainer}>
                 <p>Users have requested the following items to be added to the database:</p>
+                <br />
+                <div>{requestedItems.map((item, index) => (<div key={index} style={{ textAlign: "center", paddingBottom: 15 }}><p style={{ fontSize: 18 }}>{item}</p></div>))}</div>
             </div>
             <div style={addItemContainer}>
                 <p>Add an item to the database:</p>
