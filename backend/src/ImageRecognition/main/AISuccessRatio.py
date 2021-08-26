edff00452d2c7e15bfc1cefe698f9c0fde09a000
Keval157 <u18012559@tuks.co.za>
2021-08-26 21:17:30 +0200

    Added documentation for the function.

--- backend/src/ImageRecognition/main/AISuccessRatio.py
+++ backend/src/ImageRecognition/main/AISuccessRatio.py
@@ -10,9 +10,17 @@ dynamodb = boto3.resource('dynamodb')
 table_name = 'ScannedItems'
 table = dynamodb.Table(table_name)
 
+"""
+This lambda function takes in information of the item that was scanned. 
+The name of the item and whether the user deems if the scan was correct or not.
+A date is also generated of when the user provides this feedback.
+The function then adds the data to the database.
+The data will be simplified and shown to the admin in the ViewSuccessRatio function.
+"""
+
 
 def ai_success_ratio(event, context):
-    #is_correct is a boolean, either true or false
+    # is_correct is a boolean, either true or false
     is_correct = event['isCorrect']
     food_item = event['FoodItem']
     date = datetime.today()
