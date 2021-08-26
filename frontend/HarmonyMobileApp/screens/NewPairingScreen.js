71e6a4d002f59eae7366d4a615b0dd58b12fdd1b
Jd Perumal <u18219285@tuks.co.za>
2021-08-26 21:48:09 +0200

    Update NewPairingScreen.js
    Implemented header component

--- frontend/HarmonyMobileApp/screens/NewPairingScreen.js
+++ frontend/HarmonyMobileApp/screens/NewPairingScreen.js
@@ -1,9 +1,30 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import { Header } from "react-native-elements";
+import styles from "../styles";
 
 function NewPairingScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Header
+        statusBarProps={{ elevated: "true", backgroundColor: "black" }}
+        placement="left"
+        centerComponent={
+          <Text style={{
+            fontFamily: "sans-serif-light",
+            fontSize: 35,
+            fontWeight: "bold",
+            textAlignVertical: "center",
+          }}> Create New Pairing</Text>
+        }
+        containerStyle={{
+          backgroundColor: "white",
+        }}
+      />
       <Text>New pairing Screen</Text>
     </View>
   );
