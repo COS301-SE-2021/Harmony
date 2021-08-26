ac09de8d58a02942c4f57b21c621499fd99a5b2d
Jd Perumal <u18219285@tuks.co.za>
2021-08-27 00:53:15 +0200

    Implemented mock API request

--- frontend/HarmonyMobileApp/screens/NewPairingScreen.js
+++ frontend/HarmonyMobileApp/screens/NewPairingScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from 'react-native';
 import { Header } from "react-native-elements";
 import styles from "../styles";
@@ -7,56 +7,18 @@ import SearchableDropdown from 'react-native-searchable-dropdown';
 
 function NewPairingScreen() {
 
-  const items = [
-    {
-      id: 1,
-      name: 'JavaScript',
-    },
-    {
-      id: 2,
-      name: 'Java',
-    },
-    {
-      id: 3,
-      name: 'Ruby',
-    },
-    {
-      id: 4,
-      name: 'React Native',
-    },
-    {
-      id: 5,
-      name: 'PHP',
-    },
-    {
-      id: 6,
-      name: 'Python',
-    },
-    {
-      id: 7,
-      name: 'Go',
-    },
-    {
-      id: 8,
-      name: 'Swift8',
-    },
-    {
-      id: 9,
-      name: 'Swift9',
-    },
-    {
-      id: 10,
-      name: 'Swift10',
-    },
-    {
-      id: 11,
-      name: 'Swift11',
-    },
-  ];
-
   const [selectedItem, setSelectedItem] = useState({
     name: "Select your food...",
   });
+  const [data, setData] = useState([]);
+  const API_URL = "https://jsonplaceholder.typicode.com/users";
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => alert(error))
+  }, []);
+
 
   return (
     <View
@@ -88,7 +50,7 @@ function NewPairingScreen() {
         itemStyle={localStyles.itemStyle}
         itemTextStyle={{ color: "#6e6869" }}
         itemsContainerStyle={localStyles.itemsContainerStyle}
-        items={items}
+        items={data}
         resetValue={false}
         textInputProps={
           {
