56f9ddf83f3dd2b0ed6c9fd4a05bc0da5c16895e
Keval157 <u18012559@tuks.co.za>
2021-08-27 14:02:38 +0200

    Added documentation. Changed equality operator to use a string instead of a boolean data type.

--- backend/src/ManagePairings/main/ViewRatioData.py
+++ backend/src/ManagePairings/main/ViewRatioData.py
@@ -1,8 +1,5 @@
 import json
 import boto3
-from datetime import date
-import uuid
-from botocore.exceptions import ClientError
 
 dynamodb = boto3.resource('dynamodb')
 
@@ -11,7 +8,9 @@ table_name = 'ScannedItems'
 table = dynamodb.Table(table_name)
 
 """
-
+This function takes the data that is stored in the database of the correctly and
+incorrectly identified items as given by a general user and calculates a ratio of correct vs incorrect for 
+an admin user to view.
 """
 
 
@@ -23,13 +22,16 @@ def view_ratio_data(event, context):
 
     true_count = 0
     false_count = 0
+
     for i in itemresponse:
-        if i['CorrectlyIdentified'] == True:
+
+        if i['CorrectlyIdentified'] == "True":
             true_count = true_count + 1
         else:
             false_count = false_count + 1
 
-    calc_ratio = true_count/false_count
+    calc_ratio = true_count / false_count
+
     print(calc_ratio)
     print(totalScanned)
     return 0
