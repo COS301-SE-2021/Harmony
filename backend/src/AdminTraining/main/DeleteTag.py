90ada5a5d6e33cdc0e34f20161d9720ef9ec1bfd
Arshad Sacoor <arshadsacoor@me.com>
2021-08-27 18:56:23 +0200

    Updated Delete Tag API Function

--- backend/src/AdminTraining/main/DeleteTag.py
+++ backend/src/AdminTraining/main/DeleteTag.py
@@ -2,13 +2,13 @@ import requests
 import json
 import boto3
 def create_tag(event, context):
-    
-    tagID = "c4791196-b1ad-4dba-b06a-4e2d2058ad08"
+
+    tagID = "InsertTagID"
     url_semi = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/tags/"
     url = url_semi + tagID
 
     headers = {
-        'Training-Key': '660f5ff2178347299730d1dbee11d350',
+        'Training-Key': '',
 
     }
 
