02ab0880b0e8b6a5bd9bef2a6684bb46ed35ab58
Jd Perumal <u18219285@tuks.co.za>
2021-08-28 16:18:58 +0200

    Implemented FAB groups
    After clicking the fab the user has two options, either create new pairing or request a new item

--- frontend/HarmonyMobileApp/Components/FABNew.js
+++ frontend/HarmonyMobileApp/Components/FABNew.js
@@ -1,18 +1,48 @@
 import React from "react";
 import { useNavigation } from '@react-navigation/native';
-import { FAB } from 'react-native-paper';
-import styles from "../styles";
-
+import { FAB, Portal, Provider } from 'react-native-paper';
+import { MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
 //FAB: Floating action button
 export default function FABNew() {
     const navigation = useNavigation();
 
+    const [state, setState] = React.useState({ open: false });
+    const onStateChange = ({ open }) => setState({ open });
+    const { open } = state;
+    // onPress={() => navigation.navigate("NewPairing")}
+
     return (
-        <FAB
-            style={styles.fab}
-            icon="plus"
-            onPress={() => navigation.navigate("NewPairing")}
-        />
+        <Provider>
+            <Portal>
+                <FAB.Group
+                    fabStyle={{
+                        backgroundColor: '#03A9F4',
+                    }}
+                    open={open}
+                    icon={open ? 'close' : 'plus'}
+                    actions={[
+                        {
+                            icon: props => <FontAwesome5 name="hamburger" size={24} color="gray" />,
+                            label: 'Request new item',
+                            onPress: () => console.log('Pressed notifications'),
+                            small: false,
+                        },
+                        {
+                            icon: props => <MaterialIcons name="fastfood" size={24} color="gray" />,
+                            label: 'Create new pairing',
+                            onPress: () => console.log('Pressed notifications'),
+                            small: false,
+                        },
+                    ]}
+                    onStateChange={onStateChange}
+                    onPress={() => {
+                        if (open) {
+                            // do something if the speed dial is open
+                        }
+                    }}
+                />
+            </Portal>
+        </Provider>
     );
 }
 
