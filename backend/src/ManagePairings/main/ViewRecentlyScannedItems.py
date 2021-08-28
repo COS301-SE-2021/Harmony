73c5184d1627a80c7f83c24d3a80b64ec7fe67a5
Keval157 <u18012559@tuks.co.za>
2021-08-28 22:28:22 +0200

    Implemented code to tally up the amount of scans done on a given day in the past seven days for an admin to view.

--- backend/src/ManagePairings/main/ViewRecentlyScannedItems.py
+++ backend/src/ManagePairings/main/ViewRecentlyScannedItems.py
@@ -11,11 +11,18 @@ table = dynamodb.Table(table_name)
 
 
 def view_recently_scanned_items(event, context):
-
     # add entire table to allresponse
     allresponse = table.scan()
+    all_scanned_items = allresponse['Items']
 
-    # this takes the current time and subtract 7 days to get a range of the scanned
-    d = date.today() - timedelta(days=7)
+    date_count_array = [0, 0, 0, 0, 0, 0, 0]
+    for k in range(7):
 
+        for i in all_scanned_items:
+            # this takes the current time and subtract k days to get a range of the scanned
+            d = date.today() - timedelta(days=k + 1)
+            print(i['DateScanned'])
+            if i['DateScanned'] == str(d):
+                date_count_array[6 - k] = date_count_array[6 - k] + 1
+                
     return 0
