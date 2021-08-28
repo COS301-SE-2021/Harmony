a9ecf4d2e868145b257d89b4bd608f61f3c9c627
Keval157 <u18012559@tuks.co.za>
2021-08-28 22:40:19 +0200

    Added documentation and got the total scanned got.

--- backend/src/ManagePairings/main/ViewRecentlyScannedItems.py
+++ backend/src/ManagePairings/main/ViewRecentlyScannedItems.py
@@ -9,20 +9,31 @@ dynamodb = boto3.resource('dynamodb')
 table_name = 'ScannedItems'
 table = dynamodb.Table(table_name)
 
+"""
+This function tallies up the amount of scans all the users have done on a given day in the past k=7 days.
+The admin can use this data to populate a line graph of the amount of times items are being scanned.
+"""
+
 
 def view_recently_scanned_items(event, context):
     # add entire table to allresponse
     allresponse = table.scan()
     all_scanned_items = allresponse['Items']
 
+    # initialise count to 0 for all 7 days.
     date_count_array = [0, 0, 0, 0, 0, 0, 0]
+
+    # If we want to extend/change the days we can change the range in the for-loop as well as the relevent subtraction
+    # which is found in the for loop.
     for k in range(7):
 
         for i in all_scanned_items:
             # this takes the current time and subtract k days to get a range of the scanned
             d = date.today() - timedelta(days=k + 1)
-            print(i['DateScanned'])
             if i['DateScanned'] == str(d):
                 date_count_array[6 - k] = date_count_array[6 - k] + 1
-                
+
+    # Gets the total scans for all 7 days.
+    total_scans = sum(date_count_array)
+
     return 0
