b75f9b288f0de04d8379e54551f8aa750cb1e3cd
Keval157 <u18012559@tuks.co.za>
2021-08-28 16:45:43 +0200

    Simplified calculation for adding to most pairings and formatted the response.

--- backend/src/ManagePairings/main/ViewMostFavouritePairings.py
+++ backend/src/ManagePairings/main/ViewMostFavouritePairings.py
@@ -20,49 +20,36 @@ def view_most_favourite_pairings(event, context):
     users_response = table.scan()
     users = users_response['Items']
 
-    # array containing current pairing id's that were favourite
-    id_array = []
-
-    # Count of the number of times the corresponding favourite paring was found in favourites
-    count_array = []
-
-    for pairing_count in pairings:
-        pid = pairing_count['PID']
-        id_array.append(pid)
-        count_array.append(0)
+    initialize_count(pairings)
 
     # step through the users and if the favourite is in the users row, add 1 to that corresponding item count.
     for specific_user in users:
         for user_fav_pairings in specific_user['FavouritePairings']:
-            for k in range(len(id_array)):
-                if user_fav_pairings == id_array[k]:
-                    count_array[k] = count_array[k] + 1
+            for k in pairings:
+                if user_fav_pairings == k['PID']:
+                    k['Count'] = k['Count'] + 1
                     break
 
-    bubble_sort(id_array, count_array)
+    sortedresponse = sortbycount(pairings)
+    return{
+        "StatusCode": 200,
+        "Data": sortedresponse
+    }
 
-    for i in range(len(id_array)):
-        print(id_array[i])
-        print(count_array[i])
-    return 0
+
+# this functions returns the json value we will want to sort by
+def count_func(value):
+    return value["Count"]
 
 
-# Now do a bubble sort to sort data from largest to smallest
-# We do the sort manually instead of calling the built in sort function because
-# we need both the parallel arrays to be in the correct order as they relate to each other.
-def bubble_sort(id_array, count_array):
+def sortbycount(response):
+    # sort by count in descending order
+    sortedResponse = sorted(response, key=count_func, reverse=True)
+    return sortedResponse
 
-    len_array = len(id_array)
 
-    # Traverse through all array elements
-    for i in range(len_array - 1):
+def initialize_count(response):
+    for i in response:
+        i['Count'] = 0
 
-        # Last i elements are already in place
-        for j in range(0, len_array - i - 1):
-
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            if count_array[j] < count_array[j + 1]:
-                id_array[j], id_array[j + 1] = id_array[j + 1], id_array[j]
-                count_array[j], count_array[j + 1] = count_array[j + 1], count_array[j]
+    return response
