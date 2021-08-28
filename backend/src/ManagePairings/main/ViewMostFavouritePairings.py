3167c81fe548af1acfacf3c11f42bcddd90fcf29
Keval157 <u18012559@tuks.co.za>
2021-08-28 14:22:20 +0200

    Step through users table and add 1 to corresponding list if they're favorited to keep count.

--- backend/src/ManagePairings/main/ViewMostFavouritePairings.py
+++ backend/src/ManagePairings/main/ViewMostFavouritePairings.py
@@ -10,21 +10,41 @@ table = dynamodb.Table(table_name)
 pairing_name = 'Pairings'
 pairing_table = dynamodb.Table(pairing_name)
 
-def view_most_favourite_pairings(event, context):
 
+def view_most_favourite_pairings(event, context):
+    # get the pairings in the database
     pairing_response = pairing_table.scan()
     pairings = pairing_response['Items']
+
+    # get the users data - which contains which favourites they have.
+    users_response = table.scan()
+    users = users_response['Items']
+
     # array containing current pairing id's that were favourite
     lst = []
 
-    #Count of the number of times the corresponding favourite paring was found in favourites
+    # Count of the number of times the corresponding favourite paring was found in favourites
     lstcount = []
 
     for pairing_count in pairings:
         pid = pairing_count['PID']
         lst.append(pid)
         lstcount.append(1)
+
+    # step through the users and if the favourite is in the users row, add 1 to that corresponding item count.
+    for specific_user in users:
+        for user_fav_pairings in specific_user['FavouritePairings']:
+            for k in lst:
+                if user_fav_pairings == lst[k]:
+                    lstcount[k] = lstcount[k] + 1
+
+    # Now do a bubble sort to sort data from largest to smallest
+    # We do the sort manually instead of calling the built in sort function because
+    # we need both the parallel arrays to be in the correct order as they relate to each other.
+
     return 0
+
+
 """
     itemfound = False
 
@@ -39,5 +59,3 @@ def view_most_favourite_pairings(event, context):
             if itemfound == False:
                 lst.append(user_fav_pairings)
                 lst.append(1)"""
-
-
