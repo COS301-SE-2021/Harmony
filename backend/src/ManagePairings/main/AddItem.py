667b6f680a32c1b72f3a389735cde0bffd5d4629
Keval157 <u18012559@tuks.co.za>
2021-09-01 15:21:44 +0200

    Add image to S3 bucket function created.

--- backend/src/ManagePairings/main/AddItem.py
+++ backend/src/ManagePairings/main/AddItem.py
@@ -1,13 +1,17 @@
 import json
 import boto3
 import uuid
+import base64
 from botocore.exceptions import ClientError
 
 table_name = 'Foods'
 
-client = boto3.resource('dynamodb')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(table_name)
 
-table = client.Table(table_name)
+client = boto3.client('s3')
+s3 = boto3.resource('s3')
+bucket_name = 'harmonynewitem'
 
 """
 Adds an item to the database via the admin webpage.
@@ -16,7 +20,6 @@ Adds an item to the database via the admin webpage.
 
 
 def add_item(event, context):
-
     name = event['ItemName']
     description = event['ItemDescription']
     tags = event['ItemTags']
@@ -40,3 +43,15 @@ def add_item(event, context):
             print(e.response['Error']['Message'])
 
             return json.dumps({'StatusCode': 400})
+
+
+def add_image_to_s3(base64image, imageid):
+    imgdata = base64.b64decode(base64image)
+
+    file_name_with_extension = f'newitem/{imageid}.jpg'
+    obj = s3.Object(bucket_name, file_name_with_extension)
+    obj.put(Body=imgdata, ContentType='image/jpeg')
+    location = boto3.client('s3').get_bucket_location(Bucket=bucket_name)['LocationConstraint']
+    # get object url
+    object_url = "https://%s.s3-%s.amazonaws.com/%s" % (bucket_name, location, file_name_with_extension)
+    return object_url
