0d06a9633a958a1dc52074fe3b3ac3e9bd935d74
Keval157 <u18012559@tuks.co.za>
2021-09-05 17:00:21 +0200

    Got business user data required for processing the add pairing. Added documentation.

--- backend/src/BusinessAPIs/main/CreateBusinessPairing.py
+++ backend/src/BusinessAPIs/main/CreateBusinessPairing.py
@@ -1,12 +1,22 @@
 import json
 import boto3
 import uuid
+from botocore.exceptions import ClientError
 
 dynamodb = boto3.resource('dynamodb')
 
 # use the DynamoDB object to select our table
 table_name = 'BusinessPairings'
 table = dynamodb.Table(table_name)
+
+business_user_table_name = 'BusinessUsers'
+business_user_table = dynamodb.Table(business_user_table_name)
+
+"""
+This function adds the business users pairing to the corresponding table.
+A check needs to occur in the business user table to ensure the business is not surpassing their account limit.
+If the user paid for 5 adverts they should only be able to create 5 pairings.
+"""
 
 
 def create_business_pairing(event, context):
@@ -15,6 +25,14 @@ def create_business_pairing(event, context):
     food_desc = event['FoodDesc']
     food_item = event['FoodItem']
     price = event['Price']
+    businessID = event['BID']
+
+    """Gets the business user data that we will need to process before they can add their pairing."""
+    try:
+        busines_user_data = table.get_item(Key={'BID': businessID})
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return {"StatusCode": 400}
 
     # generate unique id for business pairing
     bpID = uuid.uuid4().hex
@@ -22,7 +40,9 @@ def create_business_pairing(event, context):
     # need to have an array of locations of stores.
     # Need to keep track of it because the business is limited to a specific number of locations otherwise they pay
     # more.
-    location_array = event['Location']
+    #location_array = event['Location']
+
+    # TODO: A check if the business user is not surpassing their current account limit (Checks the user table)
 
     # write data for new pairing to the DynamoDB table using the object we instantiated and save response in a variable
     table.put_item(
