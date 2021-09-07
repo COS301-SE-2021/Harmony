b382353eb8b69500c29a569316c189287498966d
Arshad Sacoor <arshadsacoor@me.com>
2021-09-07 20:06:52 +0200

    Added New Form Component and Updated Package files

---
+++ frontend/WebAppHarmony/src/components/MaterialUIFormSubmit.js
@@ -0,0 +1,107 @@
+import React, { useReducer } from "react";
+import { Button, Icon, TextField, Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+export function MaterialUIFormSubmit(props) {
+    const useStyles = makeStyles(theme => ({
+        button: {
+            margin: theme.spacing(1)
+        },
+        leftIcon: {
+            marginRight: theme.spacing(1)
+        },
+        rightIcon: {
+            marginLeft: theme.spacing(1)
+        },
+        iconSmall: {
+            fontSize: 20
+        },
+        root: {
+            padding: theme.spacing(3, 2)
+        },
+        container: {
+            display: "flex",
+            flexWrap: "wrap"
+        },
+        textField: {
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+            width: 400
+        }
+    }));
+
+    const [formInput, setFormInput] = useReducer(
+        (state, newState) => ({ ...state, ...newState }),
+        {
+            name: "",
+            email: ""
+        }
+    );
+
+    const handleSubmit = evt => {
+        evt.preventDefault();
+
+        let data = { formInput };
+
+        fetch("https://pointy-gauge.glitch.me/api/form", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(response => console.log("Success:", JSON.stringify(response)))
+            .catch(error => console.error("Error:", error));
+    };
+
+    const handleInput = evt => {
+        const name = evt.target.name;
+        const newValue = evt.target.value;
+        setFormInput({ [name]: newValue });
+    };
+
+    const classes = useStyles();
+
+    console.log(props);
+
+    return (
+        <div>
+            <Paper className={classes.root}>
+                <Typography variant="h5" component="h3">
+                    {props.formName}
+                </Typography>
+                <Typography component="p">{props.formDescription}</Typography>
+
+                <form onSubmit={handleSubmit}>
+                    <TextField
+                        label="TagID"
+                        id="margin-normal"
+                        name="name"
+                        defaultValue={formInput.email}
+                        className={classes.textField}
+                        helperText="Enter TagID "
+                        onChange={handleInput}
+                    />
+                    <TextField
+                        label="Image URL"
+                        id="margin-normal"
+                        name="email"
+                        defaultValue={formInput.name}
+                        className={classes.textField}
+                        helperText="e.g. .png"
+                        onChange={handleInput}
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        className={classes.button}
+                    >
+                        Submit <Icon className={classes.bottomIcon}></Icon>
+                    </Button>
+                </form>
+            </Paper>
+        </div>
+    );
+}
