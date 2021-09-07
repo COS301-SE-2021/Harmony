1a1da1b51b975d8ebef5d82d5c01f3b445b9fcb4
Arshad Sacoor <arshadsacoor@me.com>
2021-09-07 14:07:06 +0200

    Created Form for Train AI

--- frontend/WebAppHarmony/src/pages/tables/Tables.js
+++ frontend/WebAppHarmony/src/pages/tables/Tables.js
@@ -2,10 +2,30 @@ import PageTitle from "../../components/PageTitle";
 import React, {useEffect, useState} from "react";
 import MUIDataTable from "mui-datatables";
 import axios from 'axios';
-import { Grid } from "@material-ui/core";
+import {Container, Button, Grid, TextField} from "@material-ui/core";
+import  { makeStyles } from "@material-ui/core";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
+})
+
 
 export default function DataTable() {
+  const classes = useStyles()
+  const [tag, setTag]  = useState('');
   const [posts, setPost] = useState([]);
+
+  const handleSubmit  = (e) => {
+    e.preventDefault();
+  }
+
+
+
   let signal = axios.CancelToken.source();
 
   useEffect(() => {
@@ -43,20 +63,49 @@ export default function DataTable() {
     },
   };
 
-
   return (
       <>
         <PageTitle title="Train AI"/>
         <Grid container spacing={4}>
           <Grid item xs={12}>
             <MUIDataTable
-                title={"Posts"}
+                title={"Food Tags"}
                 data={posts}
                 columns={columns}
                 options={options}
             />
           </Grid>
         </Grid>
+
+        <form onSubmit={handleSubmit}>
+          <Grid container>
+            <Grid item xs={3}>
+              <TextField
+                  onChange={(e) => setTag(e.target.value)}
+                  className={classes.field}
+                  label="New Tag"
+                  variant="outlined"
+                  color = "secondary"
+                  fullWidth
+                  required
+              />
+            </Grid>
+
+          <Grid item xs={1}>
+            <Button
+              onClick={() => console.log('uouclocked')}
+              type="submit"
+              color="primary"
+              variant="contained"
+              endIcon={<KeyboardArrowRightIcon/>}
+            >
+              Submit
+          </Button>
+          </Grid>
+          </Grid>
+        </form>
+
+
         <Grid container spacing={4}>
           <Grid item xs={12}>
             <MUIDataTable
