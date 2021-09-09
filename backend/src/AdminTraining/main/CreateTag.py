f83026dff6e17f04f4647bcc968ba930f1897adb
Keval157 <u18012559@tuks.co.za>
2021-09-09 19:37:02 +0200

    Wrapped NewImageUrl.py in a lambda function. Adjusted input for CreateTag.py

--- backend/src/AdminTraining/main/CreateTag.py
+++ backend/src/AdminTraining/main/CreateTag.py
@@ -6,11 +6,9 @@ import boto3
 def create_tag(event, context):
     # create tag : the tag name will come from the user input
 
-    #tagname = event['TagName']
-
-    tag = "tag_name"
+    tagname = event['TagName']
     url_semi = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/tags?name="
-    url = url_semi + tag
+    url = url_semi + tagname
 
     headers = {
         'Training-Key': '',
--- backend/src/AdminTraining/main/NewImageUrl.py
+++ backend/src/AdminTraining/main/NewImageUrl.py
@@ -1,30 +1,31 @@
 import requests
 
-# Take Url and TagID goes into body
-url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/images/urls"
 
+def new_image_url(event, context):
+    # Take Url and TagID goes into body
+    # url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/images/urls"
+    url = event['URL']
+    tagid = ['TagID']
 
+    headers = {
+        'Training-Key': '',
+        'Content-Type': 'application/json'
 
-headers = {
-    'Training-Key': '',
-    'Content-Type': 'application/json'
+    }
 
-}
-
-body ={
+    body = {
         "images": [
             {
-                "url": "UrlImage"
+                "url": url
             }
         ],
         "tagIds": [
-            "TagId"
+            tagid
         ]
     }
 
+    response = requests.post(url, headers=headers, json=body)
 
-response = requests.post(url, headers=headers, json=body)
-
-# load the json data
-data = response.json()
-print(data)
+    # load the json data
+    data = response.json()
+    print(data)
