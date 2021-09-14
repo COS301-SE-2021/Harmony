7304aa84b77a55d9d5bfb1334ab97ee5dd8badf5
Keval157 <u18012559@tuks.co.za>
2021-09-14 21:41:11 +0200

    GetAdsToModerate.py return is completed.

--- backend/src/BusinessAPIs/main/GetAdsToModerate.py
+++ backend/src/BusinessAPIs/main/GetAdsToModerate.py
@@ -4,9 +4,19 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 
 # use the DynamoDB object to select our table
-table_name = 'BusinessPairings'
+table_name = 'RequestAdverts'
 table = dynamodb.Table(table_name)
+
+"""
+Returns all the items that have been requested to be added to the advert database by a business user.
+"""
 
 
 def get_ads_to_moderate(event, context):
-    return {"StatusCode": 200}
+    allresponse = table.scan()
+    response = allresponse['Items']
+
+    return {
+        "StatusCode": 200,
+        "Data": response
+    }
