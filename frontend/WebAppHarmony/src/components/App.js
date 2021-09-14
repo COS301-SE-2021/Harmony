c1304897e559fe6a088b004bbcba4b5411f67fff
Arshad Sacoor <arshadsacoor@me.com>
2021-09-14 06:58:28 +0200

    Added Cognito to UserContext.js

--- frontend/WebAppHarmony/src/components/App.js
+++ frontend/WebAppHarmony/src/components/App.js
@@ -1,76 +1,79 @@
-import React from "react";
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
-
+import React, {useState} from "react";
+import { HashRouter, Route, Switch, Redirect, Router, useHistory } from "react-router-dom";
+import { Auth, Hub, Amplify } from 'aws-amplify'
+import awsconfig from '../aws-exports'
+import {AmplifySignOut, withAuthenticator} from "@aws-amplify/ui-react";
 // components
 import Layout from "./Layout";
+import { useUserState } from "../context/UserContext";
 
 // pages
 import Error from "../pages/error";
 import Login from "../pages/login";
-
+Amplify.configure(awsconfig)
 // context
-import { useUserState } from "../context/UserContext";
 
 export default function App() {
-  // global
-  var { isAuthenticated } = useUserState();
+    // global
+    var { isAuthenticated } = useUserState();
 
-  return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/" render={() => <Redirect to="/app/dashboard" />} />
-        <Route
-          exact
-          path="/app"
-          render={() => <Redirect to="/app/dashboard" />}
-        />
-        <PrivateRoute path="/app" component={Layout} />
-        <PublicRoute path="/login" component={Login} />
-        <Route component={Error} />
-      </Switch>
-    </HashRouter>
-  );
-
-  // #######################################################################
-
-  function PrivateRoute({ component, ...rest }) {
     return (
-      <Route
-        {...rest}
-        render={props =>
-          isAuthenticated ? (
-            React.createElement(component, props)
-          ) : (
-            <Redirect
-              to={{
-                pathname: "/login",
-                state: {
-                  from: props.location,
-                },
-              }}
-            />
-          )
-        }
-      />
+        <HashRouter>
+            <Switch>
+                <Route exact path="/" render={() => <Redirect to="/app/dashboard" />} />
+                <Route
+                    exact
+                    path="/app"
+                    render={() => <Redirect to="/app/dashboard" />}
+                />
+                <PrivateRoute path="/app" component={Layout} />
+                <PublicRoute path="/login" component={Login} />
+                <Route component={Error} />
+            </Switch>
+        </HashRouter>
     );
-  }
 
-  function PublicRoute({ component, ...rest }) {
-    return (
-      <Route
-        {...rest}
-        render={props =>
-          isAuthenticated ? (
-            <Redirect
-              to={{
-                pathname: "/",
-              }}
+    // #######################################################################
+
+    function PrivateRoute({ component, ...rest }) {
+        return (
+            <Route
+                {...rest}
+                render={props =>
+                    isAuthenticated ? (
+                        React.createElement(component, props)
+                    ) : (
+                        <Redirect
+                            to={{
+                                pathname: "/login",
+                                state: {
+                                    from: props.location,
+                                },
+                            }}
+                        />
+                    )
+                }
             />
-          ) : (
-            React.createElement(component, props)
-          )
-        }
-      />
-    );
-  }
+        );
+    }
+
+    function PublicRoute({ component, ...rest }) {
+        return (
+            <Route
+                {...rest}
+                render={props =>
+                    isAuthenticated ? (
+                        <Redirect
+                            to={{
+                                pathname: "/",
+                            }}
+                        />
+                    ) : (
+                        React.createElement(component, props)
+                    )
+                }
+            />
+        );
+    }
 }
+
--- frontend/WebAppHarmony/src/context/UserContext.js
+++ frontend/WebAppHarmony/src/context/UserContext.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Auth } from 'aws-amplify'
 
 var UserStateContext = React.createContext();
 var UserDispatchContext = React.createContext();
@@ -57,10 +58,18 @@ function loginUser(dispatch, login, password, history, setIsLoading, setError) {
     setTimeout(() => {
       localStorage.setItem('id_token', 1)
       setError(null)
-      setIsLoading(false)
-      dispatch({ type: 'LOGIN_SUCCESS' })
+      Auth.signIn(login,password).then(user => {
+        setIsLoading(false)
+        dispatch({ type: 'LOGIN_SUCCESS' })
+        history.push('/app/dashboard')
+      }).catch(error=> {
+        dispatch({ type: "LOGIN_FAILURE" });
+        setError(true);
+        setIsLoading(false);
+      })
 
-      history.push('/app/dashboard')
+
+
     }, 2000);
   } else {
     dispatch({ type: "LOGIN_FAILURE" });
@@ -70,7 +79,18 @@ function loginUser(dispatch, login, password, history, setIsLoading, setError) {
 }
 
 function signOut(dispatch, history) {
-  localStorage.removeItem("id_token");
-  dispatch({ type: "SIGN_OUT_SUCCESS" });
-  history.push("/login");
+
+  Auth.signOut().then(() => {
+    dispatch({ type: "SIGN_OUT_SUCCESS" });
+    history.push("/login");
+  }).catch(()=>{
+
+  })
+
 }
+
+function test(){
+
+}
+
+
