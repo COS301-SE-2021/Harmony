06a52f294d706d0ac5b4beafe628facde3092d94
Keval157 <u18012559@tuks.co.za>
2021-09-14 23:41:19 +0200

    Write to table if the pairing is accepted is completed.

--- backend/src/BusinessAPIs/main/ModerateAdvert.py
+++ backend/src/BusinessAPIs/main/ModerateAdvert.py
@@ -4,9 +4,28 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 
 # use the DynamoDB object to select our table
-table_name = 'BusinessPairings'
-table = dynamodb.Table(table_name)
+request_adverts_table_name = 'RequestAdverts'
+request_adverts_table = dynamodb.Table(request_adverts_table_name)
 
 
 def moderate_advert(event, context):
+    # The id of the advert
+    advertID = event['AdvertID']
+
+    # Either Accept/Reject
+    status = event['Status']
+
+    if status == 'Accepted':
+        request_adverts_table.update_item(
+            TableName=request_adverts_table_name,
+            Key={
+                'RAID': advertID
+            },
+            ExpressionAttributeNames={'#V': 'AdvertStatus'},
+            ExpressionAttributeValues={':v': status},
+            UpdateExpression='SET #V = :v',
+            ReturnValues="UPDATED_NEW"
+
+        )
+    #else write rejected status and the set the cost back to 0
     return {"StatusCode": 200}
