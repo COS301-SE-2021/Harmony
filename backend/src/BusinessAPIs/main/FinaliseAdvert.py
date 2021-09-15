e8e86b45e14b4aad69f0846b00e52c3ad1369afb
Keval157 <u18012559@tuks.co.za>
2021-09-15 11:30:54 +0200

    Calculate cost funnction created.

--- backend/src/BusinessAPIs/main/FinaliseAdvert.py
+++ backend/src/BusinessAPIs/main/FinaliseAdvert.py
@@ -15,6 +15,8 @@ The configurations consist of : 1) Locations targeted.
 It then assigns the pending status to the pairing advert in the RequestAdverts database.
 This database contains the pairings that the admin user still has to approve before they can go live within the system.
 """
+
+
 def finalise_advert(event, context):
     # locations the ad will be used for.(Stores of the business)
     locations = event['Locations']
@@ -41,3 +43,19 @@ def finalise_advert(event, context):
     )
 
     return {"StatusCode": 200}
+
+
+"""
+This function calculates the cost of an advert by taking in the parameters we decided to use.
+    1) Number of locations
+    2) Number of audiences targeted.
+    3)Time limit (in days)
+    
+This data must be written to the database
+"""
+
+
+def calculate_cost(num_locations, num_audience, time_limit):
+    cost = time_limit + (1 * num_locations * time_limit) + (1 * num_audience * time_limit)
+
+    return cost
