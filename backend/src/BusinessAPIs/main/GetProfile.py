0b63978f21c63093cbe48738d9a03dc3e0044645
Keval157 <u18012559@tuks.co.za>
2021-09-15 12:12:00 +0200

    Database read of the users profile completed.

--- backend/src/BusinessAPIs/main/GetProfile.py
+++ backend/src/BusinessAPIs/main/GetProfile.py
@@ -1,11 +1,12 @@
 import json
 import boto3
+from botocore.exceptions import ClientError
 
 dynamodb = boto3.resource('dynamodb')
 
 # use the DynamoDB object to select our table
 table_name = 'BusinessUsers'
-table = dynamodb.Table(table_name)
+business_user_table = dynamodb.Table(table_name)
 
 """
 Returns the user data for the business users profile.
@@ -13,6 +14,17 @@ Returns the user data for the business users profile.
 
 
 def get_profile(event, context):
+
+    bid = event['BID']
+
+    """Gets the business user data using the business id"""
+    try:
+        business_user_data = business_user_table.get_item(Key={'BID': bid})
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return {"StatusCode": 400}
+
     return {
         "StatusCode": 200,
+        "UserData": business_user_data
     }
