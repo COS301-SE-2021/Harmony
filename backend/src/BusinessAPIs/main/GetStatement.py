d1eab2f1794a6bca81a05e410faf59af01e070e7
Keval157 <u18012559@tuks.co.za>
2021-09-15 11:37:17 +0200

    Get user data for statement processing

--- backend/src/BusinessAPIs/main/GetStatement.py
+++ backend/src/BusinessAPIs/main/GetStatement.py
@@ -1,11 +1,19 @@
 import json
 import boto3
+from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key
+from datetime import date, timedelta, datetime
+from datetime import time
+from datetime import datetime
 
 dynamodb = boto3.resource('dynamodb')
 
 # use the DynamoDB object to select our table
-table_name = 'RequestAdverts'
-table = dynamodb.Table(table_name)
+request_adverts_table_name = 'RequestAdverts'
+request_adverts_table = dynamodb.Table(request_adverts_table_name)
+
+business_user_table_name = 'BusinessUsers'
+business_user_table = dynamodb.Table(business_user_table_name)
 
 """
 Gets the statement for the business user.
@@ -13,8 +21,18 @@ Gets the statement for the business user.
 
 
 def get_statement(event, context):
-    allresponse = table.scan()
+
+    bid = event['BID']
+
+    allresponse = request_adverts_table.scan()
     response = allresponse['Items']
+
+    """Gets the business user data that we will need to process before they can add their pairing."""
+    try:
+        business_user_data = business_user_table.get_item(Key={'BID': bid})
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return {"StatusCode": 400}
 
     return {
         "StatusCode": 200,
