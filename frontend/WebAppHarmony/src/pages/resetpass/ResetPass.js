eb1af9639f0b6f01c990774c6663bb1beff3888a
Arshad Sacoor <arshadsacoor@me.com>
2021-09-16 15:02:22 +0200

    Created Reset/Forgot Password form with Styling and connected to Cognito

---
+++ frontend/WebAppHarmony/src/pages/resetpass/ResetPass.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import {
+  Grid,
+  CircularProgress,
+  Typography,
+  Button,
+  Tabs,
+  Tab,
+  TextField,
+  Fade,
+} from "@material-ui/core";
+import { withRouter } from "react-router-dom";
+import Form from './Form';
+import classnames from "classnames";
+import { Auth } from 'aws-amplify';
+
+// styles
+import useStyles from "./styles";
+import {useHistory} from "react-router-dom"
+// logo
+import logo from "./logo.svg";
+
+
+// context
+
+function ResetPass(props) {
+  var classes = useStyles();
+
+
+  return (
+    <Grid container className={classes.container}>
+      <div className={classes.logotypeContainer}>
+        <img src={logo} alt="logo" className={classes.logotypeImage} />
+        <Typography className={classes.logotypeText}>Harmony Admin</Typography>
+      </div>
+      <div className={classes.formContainer}>
+        <div className={classes.form}>
+        <React.Fragment>
+            <Form />
+        </React.Fragment>
+        </div>
+      </div>
+    </Grid>
+  );
+}
+
+export default withRouter(ResetPass);
