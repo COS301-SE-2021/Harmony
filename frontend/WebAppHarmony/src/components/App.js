6682187af8822c6588a207b993a4cff53ca705e8
Tiashka <seetaltiashka@gmail.com>
2021-09-17 14:00:34 +0200

    removed cognito from branch

--- frontend/WebAppHarmony/src/components/App.js
+++ frontend/WebAppHarmony/src/components/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { HashRouter, Route, Switch, Redirect} from "react-router-dom";
-import { Amplify } from 'aws-amplify'
-import '../aws-exports'
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+// import { Amplify } from 'aws-amplify'
+// import '../aws-exports'
 
 // components
 import Layout from "./Layout";
 import { useUserState } from "../context/UserContext";
-import aws_exports from '../aws-exports';
+// import aws_exports from '../aws-exports';
 import Error from "../pages/error";
 import Login from "../pages/login";
 import ResetPass from "../pages/resetpass";
@@ -18,68 +18,69 @@ Amplify.configure(aws_exports);
 // context
 
 export default function App() {
-    // global
-    var { isAuthenticated } = useUserState();
+  // global
+  var { isAuthenticated } = useUserState();
 
+  return (
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/app/dashboard" />} />
+        <Route
+          exact
+          path="/app"
+          render={() => <Redirect to="/app/dashboard" />}
+        />
+        {/* <PrivateRoute path="/app" component={Layout} /> */}
+        <PublicRoute path="/app" component={Layout} />
+        <PublicRoute path="/login" component={Login} />
+        <PublicRoute path="/resetpass" component={ResetPass} />
+        <PublicRoute path="/signup" component={SignUp} />
+        <Route component={Error} />
+      </Switch>
+    </HashRouter>
+  );
+
+  // #######################################################################
+
+  function PrivateRoute({ component, ...rest }) {
     return (
-        <HashRouter>
-            <Switch>
-                <Route exact path="/" render={() => <Redirect to="/app/dashboard" />} />
-                <Route
-                    exact
-                    path="/app"
-                    render={() => <Redirect to="/app/dashboard" />}
-                />
-                <PrivateRoute path="/app" component={Layout} />
-                <PublicRoute path="/login" component={Login} />
-                <PublicRoute path="/resetpass" component={ResetPass} />
-                <PublicRoute path="/signup" component={SignUp} />
-                <Route component={Error} />
-            </Switch>
-        </HashRouter>
+      <Route
+        {...rest}
+        render={props =>
+          isAuthenticated ? (
+            React.createElement(component, props)
+          ) : (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: {
+                  from: props.location,
+                },
+              }}
+            />
+          )
+        }
+      />
     );
+  }
 
-    // #######################################################################
-
-    function PrivateRoute({ component, ...rest }) {
-        return (
-            <Route
-                {...rest}
-                render={props =>
-                    isAuthenticated ? (
-                        React.createElement(component, props)
-                    ) : (
-                        <Redirect
-                            to={{
-                                pathname: "/login",
-                                state: {
-                                    from: props.location,
-                                },
-                            }}
-                        />
-                    )
-                }
+  function PublicRoute({ component, ...rest }) {
+    return (
+      <Route
+        {...rest}
+        render={props =>
+          isAuthenticated ? (
+            <Redirect
+              to={{
+                pathname: "/",
+              }}
             />
-        );
-    }
-
-    function PublicRoute({ component, ...rest }) {
-        return (
-            <Route
-                {...rest}
-                render={props =>
-                    isAuthenticated ? (
-                        <Redirect
-                            to={{
-                                pathname: "/",
-                            }}
-                        />
-                    ) : (
-                        React.createElement(component, props)
-                    )
-                }
-            />
-        );
-    }
+          ) : (
+            React.createElement(component, props)
+          )
+        }
+      />
+    );
+  }
 }
 
