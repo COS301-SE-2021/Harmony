b47c7d15986bbd1103e237b3a7f31a11ac35b419
Keval157 <u18012559@tuks.co.za>
2021-09-17 12:51:48 +0200

    Updated user logo in DB

--- backend/src/BusinessAPIs/main/UpdateUserLogo.py
+++ backend/src/BusinessAPIs/main/UpdateUserLogo.py
@@ -7,8 +7,8 @@ from botocore.exceptions import ClientError
 dynamodb = boto3.resource('dynamodb')
 
 # use the DynamoDB object to select our table
-request_adverts_table_name = 'BusinessUsers'
-request_adverts_table = dynamodb.Table(request_adverts_table_name)
+business_users_table_name = 'BusinessUsers'
+business_users_table = dynamodb.Table(business_users_table_name)
 
 client = boto3.client('s3')
 s3 = boto3.resource('s3')
@@ -22,10 +22,23 @@ It then updates the users logo.
 
 def update_user_logo(event, context):
     imageid = uuid.uuid4().hex
+    bid = event['BID']
 
     # place the image in the s3 bucket and get the link
 
     image_link = add_image_to_s3(event["Logo"], imageid)
+
+    business_users_table.update_item(
+        TableName=business_users_table_name,
+        Key={
+            'BID': bid
+        },
+        ExpressionAttributeNames={'#V': 'Logo'},
+        ExpressionAttributeValues={':v': image_link},
+        UpdateExpression='SET #V = :v',
+        ReturnValues="UPDATED_NEW"
+
+    )
     return {"StatusCode": 200}
 
 
@@ -36,8 +49,6 @@ This function adds an image to the s3 bucket and creates a link to be stored in 
 
 def add_image_to_s3(base64image, imageid):
     imgdata = base64.b64decode(base64image)
-
-
 
     file_name_with_extension = f'businessimages/{imageid}.jpg'
     obj = s3.Object(bucket_name, file_name_with_extension)
