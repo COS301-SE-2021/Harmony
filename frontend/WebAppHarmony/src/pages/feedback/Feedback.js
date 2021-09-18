cabbaa78ca371c065627376438a16191de4cb292
Arshad Sacoor <arshadsacoor@me.com>
2021-09-18 16:32:45 +0200

    Updated Feedback.js with API

--- frontend/WebAppHarmony/src/pages/feedback/Feedback.js
+++ frontend/WebAppHarmony/src/pages/feedback/Feedback.js
@@ -2,28 +2,26 @@ import PageTitle from "../../components/PageTitle";
 import React, {useEffect, useState} from "react";
 import MUIDataTable from "mui-datatables";
 import axios from 'axios';
-import {Container, Button, Grid, TextField} from "@material-ui/core";
+import {Grid} from "@material-ui/core";
 import { FormSubmit_ImageUrl } from "../../components/Forms/FormSubmit_ImageUrl";
 
 
 export default function DataTable() {
   const [posts, setPost] = useState([]);
-  const [posts1, setPost1] = useState([]);
   const [posts2, setPost2] = useState([]);
-  const [posts3, setPost3] = useState([]);
+  var [data1, setData] = useState(" ");
 
 
-
+  /**Get User feedback Table */
 
   let signal = axios.CancelToken.source();
-
   useEffect(() => {
     let isSubscribed = true;
     axios.get(`https://w3lfp6r6f7.execute-api.eu-west-1.amazonaws.com/dev/getuserfeedback`, {
       cancelToken: signal.token,
     })
         .then(res => {
-          const posts = res.data.UFID;
+          const posts = res.data.Data;
           setPost(posts);
         }).catch(err => {
       console.log(err);
@@ -32,6 +30,8 @@ export default function DataTable() {
       isSubscribed = false;
       signal.cancel('Api is being canceled');
     }
+
+
   }, []);
   const columns = [
     {label: "UFID", name: "UFID" },
@@ -43,40 +43,16 @@ export default function DataTable() {
 
 
 
+
+  /**Get Tags Table */
   useEffect(() => {
     let isSubscribed = true;
-    axios.get(`https://mocki.io/v1/9f39d9f9-2ed7-46cf-848d-2f6e9089767e`, {
+    axios.get(`https://7q0027151j.execute-api.eu-west-1.amazonaws.com/dev/gettags`, {
       cancelToken: signal.token,
     })
         .then(res => {
-          const posts1 = res.data;
-          setPost1(posts1);
-        }).catch(err => {
-      console.log(err);
-    });
-    return function cleanup() {
-      isSubscribed = false;
-      signal.cancel('Api is being canceled');
-    }
-  }, []);
-  const columns1 = [
-    {label: "UFID", name: "UFID" },
-    {label: "FoodName", name:"FoodName" },
-    {label:"Date Scanned", name:"DateScanned" },
-    {label:"Correctly Identified", name:"CorrectlyIdentified"},
-    {label:"FoodImage", name:"FoodImage"}
-  ]
 
-
-
-
-  useEffect(() => {
-    let isSubscribed = true;
-    axios.get(`https://mocki.io/v1/64dfb5a2-0cd9-4483-8dce-a2ef4db47f36`, {
-      cancelToken: signal.token,
-    })
-        .then(res => {
-          const posts2 = res.data;
+          const posts2 = res.data.data;
           setPost2(posts2);
         }).catch(err => {
       console.log(err);
@@ -91,39 +67,6 @@ export default function DataTable() {
     {label: "Tag Name", name:"name" },
     {label:"Type of Tag", name:"type" }
   ];
-
-
-
-
-
-
-
-  useEffect(() => {
-    let isSubscribed = true;
-    axios.get(`https://jsonplaceholder.typicode.com/photos`, {
-      cancelToken: signal.token,
-    })
-        .then(res => {
-          const posts3 = res.data;
-          setPost3(posts3);
-        }).catch(err => {
-      console.log(err);
-    });
-    return function cleanup() {
-      isSubscribed = false;
-      signal.cancel('Api is being canceled');
-    }
-  }, []);
-
-  const columns3 = [
-    {label: "userId", name: "albumId" },
-    {label: "ID", name:"id" },
-    {label:"title", name:"title" },
-    {label:"body", name:"url" }
-  ]
-
-
-
 
 
   const options = {
@@ -147,6 +90,7 @@ export default function DataTable() {
         <PageTitle title="User Feedback on Harmony AI"/>
         <Grid container spacing={4}>
           <Grid item xs={12}>
+
             <MUIDataTable
                 title={"User Feedback Table"}
                 data={posts}
@@ -164,7 +108,7 @@ export default function DataTable() {
               options={options}
           />
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={4}>
           <div className="App">
             <FormSubmit_ImageUrl
                 formName="Add new Image to Training Set"
