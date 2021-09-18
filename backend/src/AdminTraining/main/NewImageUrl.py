359948e56e69a06cbfea2a5f40022a7304f94e80
MohammedCarrim <u18080813@tuks.co.za>
2021-09-18 16:13:25 +0200

    fixed url to upload image to training set

--- backend/src/AdminTraining/main/NewImageUrl.py
+++ backend/src/AdminTraining/main/NewImageUrl.py
@@ -3,9 +3,9 @@ import requests
 
 def new_image_url(event, context):
     # Take Url and TagID goes into body
-    # url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/images/urls"
-    url = event['URL']
-    tagid = ['TagID']
+    url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/images/urls"
+    imageurl = event['URL']
+    tagid = event['TagID']
 
     headers = {
         'Training-Key': '',
@@ -16,7 +16,7 @@ def new_image_url(event, context):
     body = {
         "images": [
             {
-                "url": url
+                "url": imageurl
             }
         ],
         "tagIds": [
@@ -28,5 +28,4 @@ def new_image_url(event, context):
 
     # load the json data
     data = response.json()
-    return {"StatusCode": 200,
-            "data": data}
+    return {"StatusCode": 200}
