3fdd49d9ca760379660612e762796264f649d1ef
MohammedCarrim <u18080813@tuks.co.za>
2021-09-18 22:16:31 +0200

    wrapped IterationsPublish.py and IterationsUnpublish.py in lambda handler

--- backend/src/AdminTraining/main/IterationsUnpublish.py
+++ backend/src/AdminTraining/main/IterationsUnpublish.py
@@ -1,25 +1,29 @@
 import requests
 
-headers = {
-    'Training-Key': '660f5ff2178347299730d1dbee11d350',
-    'Content-Type': 'application/json'
+def unpublish_iteration(event, context):
 
-}
+    headers = {
+        'Training-Key': '',
+        'Content-Type': 'application/json'
 
-#Unpublish Iteration - Post
+    }
 
-#Given ite_id from User
+    #Unpublish Iteration - Post
 
-ite_id = "Put here"
-url_semi1 = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/iterations/"
-url_semi2 = "/publish"
+    #Given ite_id from User
 
-url1 = ite_id + url_semi1 + url_semi2
+    ite_id = event["IterId"]
+    url_semi1 = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/iterations/"
+    url_semi2 = "/publish"
+
+    url1 = url_semi1 + ite_id + url_semi2
 
 
 
-response = requests.delete(url1, headers=headers)
+    requests.delete(url1, headers=headers)
 
-# load the json data
-data = response.json()
-print(data)
+    # load the json data
+    return {
+        "StatusCode" : 200,
+        "Data" : "Iteration is unpublished"
+    }
