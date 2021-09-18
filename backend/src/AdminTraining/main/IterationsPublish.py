a3ad482f81bdfc474e55bca5d7e522713d1c51c7
Arshad Sacoor <arshadsacoor@me.com>
2021-09-18 17:16:47 +0200

    Publish Iteration Update

---
+++ backend/src/AdminTraining/main/IterationsPublish.py
@@ -0,0 +1,17 @@
+#Publish Iteration
+
+
+url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/training/projects/b2c99ecb-e43e-4a59-ac87-a189c109e267/images/urls"
+
+
+body ={
+  "type": "True"
+}
+
+response = requests.post(url, headers=headers, json=body)
+
+# load the json data
+data = response.json()
+print(data)
+
+
