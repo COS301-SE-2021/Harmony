51d2fe89f6ec01945bf83239d9a78e8a71014883
Arshad Sacoor <arshadsacoor@me.com>
2021-09-19 18:57:40 +0200

    Updated App.js

--- frontend/BusinessAppHarmony/src/components/App.js
+++ frontend/BusinessAppHarmony/src/components/App.js
@@ -1,76 +1,86 @@
 import React from "react";
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import { HashRouter, Route, Switch, Redirect} from "react-router-dom";
 
 // components
 import Layout from "./Layout";
 
+
 // pages
+import { useUserState } from "../context/UserContext";
+import aws_exports from '../aws-exports';
 import Error from "../pages/error";
 import Login from "../pages/login";
-
+import ResetPass from "../pages/resetpass";
+import SignUp from "../pages/signup";
+import { Amplify } from 'aws-amplify'
+import '../aws-exports'
+Amplify.configure(aws_exports);
 // context
-import { useUserState } from "../context/UserContext";
 
 export default function App() {
-  // global
-  var { isAuthenticated } = useUserState();
+    // global
+    var { isAuthenticated } = useUserState();
 
-  return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/" render={() => <Redirect to="/app/dashboard" />} />
-        <Route
-          exact
-          path="/app"
-          render={() => <Redirect to="/app/dashboard" />}
-        />
-        <PrivateRoute path="/app" component={Layout} />
-        <PublicRoute path="/login" component={Login} />
-        <Route component={Error} />
-      </Switch>
-    </HashRouter>
-  );
-
-  // #######################################################################
-
-  function PrivateRoute({ component, ...rest }) {
     return (
-      <Route
-        {...rest}
-        render={props =>
-          isAuthenticated ? (
-            React.createElement(component, props)
-          ) : (
-            <Redirect
-              to={{
-                pathname: "/login",
-                state: {
-                  from: props.location,
-                },
-              }}
-            />
-          )
-        }
-      />
-    );
-  }
+        <HashRouter>
+            <Switch>
+                <Route exact path="/" render={() => <Redirect to="/app/dashboard" />} />
+                <Route
+                    exact
+                    path="/app"
+                    render={() => <Redirect to="/app/dashboard" />}
+                />
+                <PrivateRoute path="/app" component={Layout} />
+                <PublicRoute path="/login" component={Login } />
 
-  function PublicRoute({ component, ...rest }) {
-    return (
-      <Route
-        {...rest}
-        render={props =>
-          isAuthenticated ? (
-            <Redirect
-              to={{
-                pathname: "/",
-              }}
-            />
-          ) : (
-            React.createElement(component, props)
-          )
-        }
-      />
+                <PublicRoute path="/resetpass" component={ResetPass} />
+                <PublicRoute path="/signup" component={SignUp} />
+                <Route component={Error} />
+            </Switch>
+        </HashRouter>
     );
-  }
+
+    // #######################################################################
+
+    function PrivateRoute({ component, ...rest }) {
+        return (
+            <Route
+                {...rest}
+                render={props =>
+                    isAuthenticated ? (
+                        React.createElement(component, props)
+                    ) : (
+                        <Redirect
+                            to={{
+                                pathname: "/login",
+                                state: {
+                                    from: props.location,
+                                },
+                            }}
+                        />
+                    )
+                }
+            />
+        );
+    }
+
+    function PublicRoute({ component, ...rest }) {
+        return (
+            <Route
+                {...rest}
+                render={props =>
+                    isAuthenticated ? (
+                        <Redirect
+                            to={{
+                                pathname: "/",
+                            }}
+                        />
+                    ) : (
+                        React.createElement(component, props)
+                    )
+                }
+            />
+        );
+    }
 }
+
