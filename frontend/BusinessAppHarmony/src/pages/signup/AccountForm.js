9a94dd79defc9a11b84a621833854dc9b0c5366d
Arshad Sacoor <arshadsacoor@me.com>
2021-09-19 18:58:36 +0200

    Created Sign Up forms

---
+++ frontend/BusinessAppHarmony/src/pages/signup/AccountForm.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+
+export class AccountForm extends Component {
+	render() {
+		const { values, handleChange } = this.props;
+		return (
+			<React.Fragment>
+				<Typography variant="h6" gutterBottom>
+					Account Details
+				</Typography>
+				<Grid container spacing={24}>
+					<Grid item xs={12}>
+						<TextField
+							required
+							id="username"
+							name="username"
+							label="Username"
+							onChange={handleChange("username")}
+							defaultValue={values.username}
+							fullWidth
+							autoComplete="username"
+						/>
+					</Grid>
+
+					<Grid item xs={12}>
+						<TextField
+							required
+							id="email"
+							name="email"
+							label="Email"
+							onChange={handleChange("email")}
+							defaultValue={values.email}
+							fullWidth
+							autoComplete="email"
+						/>
+					</Grid>
+
+					<Grid item xs={12}>
+						<TextField
+							required
+							id="password"
+							name="password"
+							label="Password"
+							onChange={handleChange("password")}
+							defaultValue={values.password}
+							fullWidth
+							type="password"
+						/>
+					</Grid>
+
+				</Grid>
+			</React.Fragment>
+		);
+	}
+}
+
+export default AccountForm;
