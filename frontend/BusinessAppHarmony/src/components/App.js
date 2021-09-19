0a756145eee8656fc3cf2b8deddc00868fff61ad
Arshad Sacoor <arshadsacoor@me.com>
2021-09-19 23:10:01 +0200

    Updated Nav Bar

--- frontend/BusinessAppHarmony/src/components/App.js
+++ frontend/BusinessAppHarmony/src/components/App.js
@@ -36,11 +36,11 @@ export default function App() {
     return (
         <HashRouter>
             <Switch>
-                <Route exact path="/" render={() => <Redirect to="/app/dashboard" />} />
+                <Route exact path="/" render={() => <Redirect to="/app/usertrends" />} />
                 <Route
                     exact
                     path="/app"
-                    render={() => <Redirect to="/app/dashboard" />}
+                    render={() => <Redirect to="/app/usertrends" />}
                 />
                 <PrivateRoute path="/app" component={Layout} />
                 <PublicRoute path="/login" component={Login} />
