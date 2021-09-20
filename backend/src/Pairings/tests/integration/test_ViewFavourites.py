012393ae225b9ab4de47f8e9ea0a997cdb91bdaa
MohammedCarrim <u18080813@tuks.co.za>
2021-09-20 08:25:20 +0200

    added benchmark testing

--- backend/src/Pairings/tests/integration/test_ViewFavourites.py
+++ backend/src/Pairings/tests/integration/test_ViewFavourites.py
@@ -1,6 +1,8 @@
 import json
 
 import pytest
+import sys
+import time
 
 from backend.src.Pairings.main import ViewFavourites
 
@@ -78,33 +80,41 @@ def incorrect_sort_type():
 
     }
 
-def test_view_favourites_with_valid_data_from_db(fixture_event):
+def test_view_favourites_with_valid_data_from_db(fixture_event, benchmark):
     data = ViewFavourites.view_favourites(fixture_event, "")
     print(data)
     assert data["StatusCode"] == 200
     assert "Data" in data
+    benchmark(lambda: time.sleep(0.01))
 
 
-def test_view_favourites_with_wrong_mealtag_data(incorrect_meal_tag):
+def test_view_favourites_with_wrong_mealtag_data(incorrect_meal_tag,benchmark):
     data = ViewFavourites.view_favourites(incorrect_meal_tag, "")
     print(data)
     assert data["StatusCode"] == 204
     assert "Data" in data
+    benchmark(lambda: time.sleep(0.01))
 
-def test_view_favourites_with_wrong_foodtag_data(incorrect_food_tag):
+
+def test_view_favourites_with_wrong_foodtag_data(incorrect_food_tag,benchmark):
     data = ViewFavourites.view_favourites(incorrect_food_tag, "")
     print(data)
     assert data["StatusCode"] == 204
     assert "Data" in data
+    benchmark(lambda: time.sleep(0.01))
 
-def test_view_favourites_with_wrong_drinktag_data(incorrect_drink_tag):
+
+def test_view_favourites_with_wrong_drinktag_data(incorrect_drink_tag,benchmark):
     data = ViewFavourites.view_favourites(incorrect_drink_tag, "")
     print(data)
     assert data["StatusCode"] == 204
     assert "Data" in data
+    benchmark(lambda: time.sleep(0.01))
 
-def test_view_favourites_with_wrong_sort_type(incorrect_drink_tag):
+
+def test_view_favourites_with_wrong_sort_type(incorrect_drink_tag,benchmark):
     data = ViewFavourites.view_favourites(incorrect_drink_tag, "")
     print(data)
     assert data["StatusCode"] == 204
     assert "Data" in data
+    benchmark(lambda: time.sleep(0.01))
--- backend/src/Pairings/tests/integration/test_ViewUserPairings.py
+++ backend/src/Pairings/tests/integration/test_ViewUserPairings.py
@@ -1,6 +1,8 @@
 import json
 
 import pytest
+import sys
+import time
 
 from backend.src.Pairings.main import ViewUserPairings
 
@@ -78,34 +80,43 @@ def incorrect_sort_type():
 
     }
 
-def test_view_userpairings_with_valid_data_from_db(fixture_event):
+def test_view_userpairings_with_valid_data_from_db(fixture_event,benchmark):
     data = ViewUserPairings.view_user_pairings(fixture_event, "")
     print(data)
     assert data["StatusCode"] == 200
     assert "Data" in data
+    benchmark(lambda: time.sleep(0.01))
 
 
-def test_view_userpairings_with_wrong_mealtag_data(incorrect_meal_tag):
+def test_view_userpairings_with_wrong_mealtag_data(incorrect_meal_tag,benchmark):
     data = ViewUserPairings.view_user_pairings(incorrect_meal_tag, "")
     print(data)
     assert data["StatusCode"] == 204
     assert "Data" in data
+    benchmark(lambda: time.sleep(0.01))
 
-def test_view_userpairings_with_wrong_foodtag_data(incorrect_food_tag):
+
+def test_view_userpairings_with_wrong_foodtag_data(incorrect_food_tag,benchmark):
     data = ViewUserPairings.view_user_pairings(incorrect_food_tag, "")
     print(data)
     assert data["StatusCode"] == 204
     assert "Data" in data
+    benchmark(lambda: time.sleep(0.01))
 
-def test_view_userpairings_with_wrong_drinktag_data(incorrect_drink_tag):
+
+def test_view_userpairings_with_wrong_drinktag_data(incorrect_drink_tag,benchmark):
     data = ViewUserPairings.view_user_pairings(incorrect_drink_tag, "")
     print(data)
     assert data["StatusCode"] == 204
     assert "Data" in data
+    benchmark(lambda: time.sleep(0.01))
 
-def test_view_userpairings_with_wrong_sort_type(incorrect_drink_tag):
+
+def test_view_userpairings_with_wrong_sort_type(incorrect_drink_tag,benchmark):
     data = ViewUserPairings.view_user_pairings(incorrect_drink_tag, "")
     print(data)
     assert data["StatusCode"] == 204
     assert "Data" in data
+    benchmark(lambda: time.sleep(0.01))
+
 
