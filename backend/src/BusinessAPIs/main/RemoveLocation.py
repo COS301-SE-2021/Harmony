fda896614167a2873d6db76ec0343de85b25c538
Keval157 <u18012559@tuks.co.za>
2021-10-02 12:11:49 +0200

    Get business user profile to be updated.

--- backend/src/BusinessAPIs/main/RemoveLocation.py
+++ backend/src/BusinessAPIs/main/RemoveLocation.py
@@ -16,4 +16,20 @@ def remove_location(event, context):
     bid = event['BID']
     location_id = event['Location_ID']
     location_name = event['Location_name']
+
+    """try:
+        business_users_table.delete_item(Key={'RID': i['RID']})
+    except ClientError as e:
+        if e.response['Error']['Code'] == "ConditionalCheckFailedException":
+            print(e.response['Error']['Message'])
+
+            return {"StatusCode": 400}"""
+
+    """Gets the business user data using the business id"""
+    try:
+        business_user_data = business_users_table.get_item(Key={'BID': bid})
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return {"StatusCode": 400}
+
     return {"StatusCode": 200}
