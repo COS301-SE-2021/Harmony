edc40a0a05879bf201dd30568c20f41589a8d52a
Keval157 <u18012559@tuks.co.za>
2021-10-03 15:53:08 +0200

    Write new registration and name to each respective table.

--- backend/src/BusinessAPIs/main/UpdateBusinessRegistration.py
+++ backend/src/BusinessAPIs/main/UpdateBusinessRegistration.py
@@ -5,11 +5,27 @@ from botocore.exceptions import ClientError
 dynamodb = boto3.resource('dynamodb')
 
 # use the DynamoDB object to select our table
-table_name = 'BusinessUsers'
-business_user_table = dynamodb.Table(table_name)
+business_user_table_name = 'BusinessUsers'
+business_user_table = dynamodb.Table(business_user_table_name)
 
 
 def update_name(event, context):
     registration = event['Registration']
+    bid = event['BID']
+
+    """
+    Writes the new business registration to the business users table.
+    """
+    business_user_table.update_item(
+        TableName=business_user_table_name,
+        Key={
+            'BID': bid
+        },
+        ExpressionAttributeNames={'#V': 'BusinessName'},
+        ExpressionAttributeValues={':v': registration},
+        UpdateExpression='SET #V = :v',
+        ReturnValues="UPDATED_NEW"
+
+    )
 
     return {"StatusCode": 200}
--- backend/src/BusinessAPIs/main/UpdateName.py
+++ backend/src/BusinessAPIs/main/UpdateName.py
@@ -5,11 +5,27 @@ from botocore.exceptions import ClientError
 dynamodb = boto3.resource('dynamodb')
 
 # use the DynamoDB object to select our table
-table_name = 'BusinessUsers'
-business_user_table = dynamodb.Table(table_name)
+business_user_table_name = 'BusinessUsers'
+business_user_table = dynamodb.Table(business_user_table_name)
 
 
 def update_name(event, context):
     new_name = event['Name']
+    bid = event['BID']
+
+    """
+    Writes the new business name to the business users table.
+    """
+    business_user_table.update_item(
+        TableName=business_user_table_name,
+        Key={
+            'BID': bid
+        },
+        ExpressionAttributeNames={'#V': 'BusinessName'},
+        ExpressionAttributeValues={':v': new_name},
+        UpdateExpression='SET #V = :v',
+        ReturnValues="UPDATED_NEW"
+
+    )
 
     return {"StatusCode": 200}
