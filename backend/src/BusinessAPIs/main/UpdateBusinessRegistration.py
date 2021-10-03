8c85be9b63528d9dc4160458d16e962594063b5f
Keval157 <u18012559@tuks.co.za>
2021-10-03 15:49:10 +0200

    Created lambda functions for UpdateName.py and UpdateBusinessRegistration.py

---
+++ backend/src/BusinessAPIs/main/UpdateBusinessRegistration.py
@@ -0,0 +1,15 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+dynamodb = boto3.resource('dynamodb')
+
+# use the DynamoDB object to select our table
+table_name = 'BusinessUsers'
+business_user_table = dynamodb.Table(table_name)
+
+
+def update_name(event, context):
+    registration = event['Registration']
+
+    return {"StatusCode": 200}
