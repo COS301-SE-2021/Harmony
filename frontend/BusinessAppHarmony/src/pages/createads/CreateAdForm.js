fc846e820c2e40b7d416f587a60ed3074a856c4a
Tiashka <seetaltiashka@gmail.com>
2021-10-03 21:37:12 +0200

    fixed the api

--- frontend/BusinessAppHarmony/src/pages/createads/CreateAdForm.js
+++ frontend/BusinessAppHarmony/src/pages/createads/CreateAdForm.js
@@ -120,6 +120,7 @@ function CreateAdForm(...props) {
             TimePeriod: vals.TimePeriod,
             FoodImage: foodImage.split(',')[1],
             DrinkImage: drinkImage.split(',')[1],
+            Radius: 50
         }
         console.log("request to submit " + JSON.stringify(request))
         fetch('https://alt0c0nrq7.execute-api.eu-west-1.amazonaws.com/dev/createbusinesspairing', {
