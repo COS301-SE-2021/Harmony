2695d95d967c47792b95879e8d39a1d6231f3c0e
Arshad Sacoor <arshadsacoor@me.com>
2021-10-04 20:35:28 +0200

    Updated Styling on Homepage

--- frontend/WebAppHarmony/src/pages/dashboard/Dashboard.js
+++ frontend/WebAppHarmony/src/pages/dashboard/Dashboard.js
@@ -70,8 +70,7 @@ export default function Dashboard(props) {
   }, []);
   return (
     <>
-      <PageTitle title="Dashboard"
-      >
+      <PageTitle title="Dashboard"/>
       <Grid container spacing={4}>
         <Grid item lg={3} md={4} sm={6} xs={12}>
           <Widget
