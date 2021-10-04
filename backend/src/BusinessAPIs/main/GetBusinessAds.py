f39d3b2c019e3f2cfb8dc389b870bbeddfbc1ab4
Keval157 <u18012559@tuks.co.za>
2021-10-04 14:49:46 +0200

    Get the adverts for a speicfic user. Calculated the time remaining on their adverts.

--- backend/src/BusinessAPIs/main/GetBusinessAds.py
+++ backend/src/BusinessAPIs/main/GetBusinessAds.py
@@ -1,6 +1,9 @@
 import json
 import boto3
 from botocore.exceptions import ClientError
+from datetime import date, timedelta, datetime
+from datetime import time
+from datetime import datetime
 
 dynamodb = boto3.resource('dynamodb')
 
@@ -15,4 +18,33 @@ def get_business_ads(event, context):
     response = business_pairing_table.scan()
     business_pairings = response["Items"]
 
-    return {"StatusCode": 200}
+    adverts = []
+    for i in business_pairings:
+        date_created = datetime.strptime(i['DateCreated'], '%Y-%m-%d')
+        if i["BID"] == bid:
+
+            today = datetime.strptime(str(date.today()), '%Y-%m-%d')
+            days_completed = (today - date_created).days
+
+            timeLimit = 30
+
+            if i['TimeLimit'] == "One Month":
+                timeLimit = 30
+            if i['TimeLimit'] == "One Day":
+                timeLimit = 1
+            if i['TimeLimit'] == "Three Months":
+                timeLimit = 90
+            if i['TimeLimit'] == "Six Months":
+                timeLimit = 180
+            if i['TimeLimit'] == "One Year":
+                timeLimit = 365
+            if i['TimeLimit'] == "One Week":
+                timeLimit = 7
+
+
+            days_remaining = timeLimit - days_completed
+            i['timeLeft'] = days_remaining
+            adverts.append(i)
+
+    return {"StatusCode": 200,
+            "Adverts": adverts}
