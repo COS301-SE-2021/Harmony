aef79c20db9683d3b96bbadc8ef1126d492961ff
Tiashka <seetaltiashka@gmail.com>
2021-10-13 11:37:45 +0200

    removed more code duplication

--- frontend/BusinessAppHarmony/src/pages/usertrends/styles.js
+++ frontend/BusinessAppHarmony/src/pages/usertrends/styles.js
@@ -12,106 +12,9 @@ export default makeStyles(theme => ({
   text: {
     marginBottom: theme.spacing(2),
   },
-  textAreaField: {
-    width: "85%",
-    height: 100,
-    padding: 15,
-    fontFamily: "sans-serif-light",
-    color: "#717171",
-  },
 
-  errorDiv: {
-    fontSize: 15,
-    marginTop: 0,
-    color: "#717171",
-  },
-  individualTextField: {
-    width: "100%",
-    height: 70,
-    float: "left"
-  },
-  multiselector: {
-    width: "75%",
-    height: 35,
-    marginLeft: 8
-  },
-  multiselectorTag: {
-    width: "75%",
-    height: 15,
-    marginLeft: 100
-  },
-  formLabel: {
-    fontSize: 20,
-    width: 150,
-    justifyContent: "space-between",
-    display: "flex",
-    marginTop: 15
-  },
-  formElements: {
-    marginTop: 20,
-    marginBottom: 20,
-    borderRadius: 15,
-    display: "flex"
-  },
-  formElementsPairingTag: {
-    marginTop: 20,
-    marginBottom: 30,
-    display: "flex",
-    width: '40%',
-  },
-  tagName: {
-    float: 'left',
-    marginRight: 15,
-    marginTop: 8,
-    fontSize: 20
-  },
-  MealContainer: {
-    marginTop: 20,
-    marginBottom: 20,
-    width: "85%",
-    margin: "auto",
-    display: "table"
-  },
-  formElementsImageContainer: {
-    marginTop: 20,
-    marginBottom: 20,
-    borderRadius: 15,
-    justifyContent: "space-around",
-    display: "flex"
-  },
-  addItemContainer: {
-    width: "95%",
-    marginLeft: 40,
-    padding: 15,
-    borderRadius: 5,
-    float: "left",
-    backgroundColor: "white",
-    textAlign: "center",
-    boxShadow: " 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.05)"
-  },
-  requestContainer: {
-    width: "25%",
-    height: 620,
-    padding: 15,
-    borderRadius: 5,
-    backgroundColor: "white",
-    float: "left",
-    textAlign: "center",
-    // margin: "auto",
-    boxShadow: " 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.05)"
-  },
   button: {
     backgroundColor: "#CECECE", height: 35, borderRadius: 10, paddingLeft: 12, paddingRight: 12
-  },
-  toggleButton: {
-    borderRadius: 10, height: 70
-  },
-
-  uploadLogoButton: {
-    backgroundColor: theme.palette.primary.main,
-    height: 25, borderRadius: 50, paddingLeft: 10, paddingRight: 10, fontSize: 10, marginLeft: 10, marginTop: -5,
-    float: "none", color: 'white',
-    boxShadow: " 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.05)"
   },
 
   buttonContainer: {
@@ -119,31 +22,6 @@ export default makeStyles(theme => ({
   },
   marginAuto: {
     margin: "auto",
-  },
-  addButton: {
-    backgroundColor: "#81b5c2", color: "white", marginLeft: 45, height: 35, borderRadius: 10, paddingLeft: 12, paddingRight: 12, width: "30%",
-    boxShadow: " 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.05)"
-
-  },
-  addLocationButton: {
-    backgroundColor: "#81b5c2", color: "white", marginLeft: 25, height: 35, borderRadius: 10, width: "60%",
-    boxShadow: " 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.05)",
-    float: "left"
-  },
-  ClearButton: {
-    backgroundColor: "#CECECE", marginLeft: 15, height: 35, borderRadius: 10, width: "20%",
-    boxShadow: " 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.05)",
-    float: "left"
-  },
-
-  ButtonContainer: {
-    float: 'left',
-    margin: 'auto',
-    width: '100%'
-  },
-
-  clear: {
-    clear: "both"
   },
   foodChip: {
     backgroundColor: "#FF6347",
@@ -175,61 +53,8 @@ export default makeStyles(theme => ({
 
 
   },
-  displayFlexJustifyCenter: {
-    display: "flex", justifyContent: "center"
-  },
-  displayFlexJustifyAround: {
-    display: "flex", justifyContent: "space-around"
-  },
-  background: {
-    flexDirection: "row", justifyContent: "space-around", paddingTop: 15, backgroundColor: "#F3F3F3",
-  },
-  greyContainer: {
-    justifyContent: "center", display: "flex", flexDirection: "row", paddingTop: 25, backgroundColor: "#F3F3F3"
-  },
-  displayFlexOnly: {
-    display: "flex"
-  },
   icon: { float: "right", height: 20, width: 20, marginTop: 15, marginBottom: 0 },
-  fontSizeMedium: {
-    fontSize: 20,
-    fontFamily: "sans-serif-light",
-    marginTop: 0
-  },
-  pageContainer: {
-    float: "right",
-    width: "95%"
-  },
-  CoordinatesContainer: {
-    float: "left",
-    width: "100%",
-    paddingRight: 10,
-    paddingLeft: 10,
-  },
-  formContainer: {
-    display: "flex",
-  },
-  errorStar: {
-    color: "red", fontSize: 25, marginRight: 5, marginTop: -10
-  },
-  outstandingBalance: {
-    color: theme.palette.primary.main,
-    textAlign: "center"
-  },
-  outstandingBalanceWord: {
-    fontSize: 15,
-    marginTop: 20,
-    color: "#717171",
-    float: "left"
-  },
-  payNowButton: {
-    width: "100%",
-    margin: "auto",
-    backgroundColor: theme.palette.primary.main,
-    fontSize: 17,
-    color: "white",
-    height: 50
-  },
+
   tableRowHeader: {
     backgroundColor: theme.palette.primary.main,
     color: "white"
